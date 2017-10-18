a08a947da7846dff3401f34d3520faad477a35c1
Sofia <davidosky3@gmail.com>
2017-10-18 18:54:25 +0100

    Update setupTermuxArch.sh
    Replaced tar with bsdtar as it's the recommended option by archlinuxarm
    Replaced instances of echo and empty echoes by printf and newlines
    Autoremoves the files in case of md5 mismatch
    Replaced mirror with os.archlinux.org
    Added more messages
    Disabled verbose on wget and apt

--- setupTermuxArch.sh
+++ setupTermuxArch.sh
@@ -1,67 +1,59 @@
 #!/data/data/com.termux/files/usr/bin/sh -e
-# Copyright 2017 (c) all rights reserved \ 
+# Copyright 2017 (c) all rights reserved \
 # by SDRausty https://sdrausty.github.io/termux-arch
-# Contributors @Soph1a7 et al.
+# Contributers @Soph1a7 et al.
 #####################################################################
 bin=startArch.sh
-mirror=mirror.archlinuxarm.org
-echo "This setup script will attempt to set Arch Linux up in your Termux environment."
-echo 
-sleep 2
-echo "When successfully completed, you will be at the bash prompt in Arch Linux in Termux."
+mirror=os.archlinuxarm.org
+#If you need to change your mirror, only change the os.archlinuxarm.org to your desired mirror location
+printf "This setup script will attempt to set Arch Linux up in your Termux environment.\n"
 sleep 1
-pkg install proot tar wget
-echo
+printf "\nWhen successfully completed, you will be at the bash prompt in Arch Linux in Termux.\n"
+sleep 1
+printf "\nThe script will now install the required tools for Arch installation\n\n"
+apt-get -qq install --yes proot wget bsdtar
+printf "\n\nNow downloading the Arch image. This may take a while depending on your internet speed, be patient.\n\n"
 mkdir -p $HOME/arch
 cd $HOME/arch
-echo
-if [ "$(uname -m)" = "aarch64" ]; then
-	wget -c http://$mirror/os/ArchLinuxARM-aarch64-latest.tar.gz.md5
-	wget -c http://$mirror/os/ArchLinuxARM-aarch64-latest.tar.gz
-	echo "Checking md5sum. This may take a while. Please be patient."
-	echo
+if [ "$(uname -m)" = "aarch64" ];then
+wget -c -q --show-progress http://$mirror/os/ArchLinuxARM-aarch64-latest.tar.gz.md5
+wget -c -q --show-progress http://$mirror/os/ArchLinuxARM-aarch64-latest.tar.gz
+printf "\nChecking md5sum. This may take a while. Please be patient.\n\n"
 if md5sum -c ArchLinuxARM-aarch64-latest.tar.gz.md5; then
-	echo
-	echo "Uncompressing ArchLinuxARM-aarch64-latest.tar.gz"
-	echo
-	echo "This will take much longer. Please be patient."
-	proot --link2symlink tar -xf ArchLinuxARM-aarch64-latest.tar.gz 2>/dev/null||:
+	printf "\nUncompressing ArchLinuxARM-aarch64-latest.tar.gz\n"
+	printf "\nThis will take much longer. Please be patient.\n"
+	proot --link2symlink bsdtar -xpf ArchLinuxARM-aarch64-latest.tar.gz 2>/dev/null||:
 else
-	echo
-	echo "ERROR md5sum mismatch! Remove $HOME/arch with all downloads and restart \`setupTermuxArch.sh\`."
-	echo
-	echo "Alternatively, change \`mirror=$mirror\` in line 7 and remove $HOME/arch with all downloads, then restart \`setupTermuxArch.sh\`. Use https://archlinuxarm.org/about/mirrors to choose an available mirror in accordance with your 🌐 geographic location." 
-	echo
+	printf "\nmd5sum missmatch! Removing the arch directory contents\n"
+	printf "\nPlease execute the script and try again\n"
+	printf "\nIf this keeps failing, please change your mirror on line 7 with an editor like nano\n"
+	printf "\nUse https://archlinuxarm.org/about/mirrors to choose an available mirror in accordance with your 🌎 geographic location.\n"
+	rm -rf $HOME/arch/*
+	printf "\nExiting...\n"
 	exit 1
 fi
-elif [ "$(uname -m)" = "armv7l" ]; then
-	wget -c http://$mirror/os/ArchLinuxARM-armv7-latest.tar.gz.md5
-	wget -c http://$mirror/os/ArchLinuxARM-armv7-latest.tar.gz
-	echo "Checking md5sum. This may take a while. Please be patient."
-	echo
+elif [ "$(uname -m)" = "armv7l" ];then
+wget -c -q --show-progress http://$mirror/os/ArchLinuxARM-armv7-latest.tar.gz.md5
+wget -c -q --show-progress http://$mirror/os/ArchLinuxARM-armv7-latest.tar.gz
+printf "\nChecking md5sum. This may take a while. Please be patient.\n\n"
 if md5sum -c ArchLinuxARM-armv7-latest.tar.gz.md5; then
-	echo
-	echo "Uncompressing ArchLinuxARM-armv7-latest.tar.gz"
-	echo
-	echo "This will take much longer. Please be patient."
-	proot --link2symlink tar -xf ArchLinuxARM-armv7-latest.tar.gz 2>/dev/null||:
+	printf "\nUncompressing ArchLinuxARM-armv7-latest.tar.gz\n"
+	printf "\nThis will take much longer. Please be patient.\n"
+	proot --link2symlink bsdtar -xpf ArchLinuxARM-armv7-latest.tar.gz 2>/dev/null||:
 else
-	echo
-	echo "ERROR md5sum mismatch! Remove $HOME/arch with all downloads and restart \`setupTermuxArch.sh\`."
-	echo
-	echo "Alternatively, change \`mirror=$mirror\` in line 7 and remove $HOME/arch with all downloads, then restart \`setupTermuxArch.sh\`. Use https://archlinuxarm.org/about/mirrors to choose an available mirror in accordance with your 🌐 geographic location." 
-	echo
+	printf "\nmd5sum missmatch! Removing the arch directory contents\n"
+	printf "\nPlease execute the script and try again\n"
+	printf "\nIf this keeps failing, please change your mirror on line 7 with an editor like nano\n"
+	printf "\nUse https://archlinuxarm.org/about/mirrors to choose an available mirror in accordance with your 🌎 geographic location.\n"
+	rm -rf $HOME/arch/*
+	printf "\nExiting...\n"
 	exit 1
 fi
 else
-	echo "ERROR Unknown architecture version! There might still be hope."
-	echo
-	echo "👉 Check for other available architectures at http://mirror.archlinuxarm.org/os/ and see if any match your device."
-	echo
-	echo "👉 If you find a match, then please submit a pull request at https://github.com/sdrausty/TermuxArch/pulls with script modifications."
-	echo
-	echo "👉 Alternatively, submit a modification request at https://github.com/sdrausty/TermuxArch/issues if you find an architecture match. Please include output from \`uname -mo\` on the device in order to expand architecture auto-detection for \`setupTermuxArch.sh\`."
-	echo
+	printf "\nERROR Unknown architecture version! There might still be hope.\n"
+	printf "\n👉 Check for other available architectures at http://mirror.archlinuxarm.org/os/ and see if any match your device.\n"
+	printf "\n👉 If you find a match, then please submit a pull request at https://github.com/sdrausty/TermuxArch/pulls with script modifications.\n"
+	printf "\n👉 Alternatively, submit a modification request at https://github.com/sdrausty/TermuxArch/issues if you find an architecture match. Please include output from \`uname -mo\` on the device in order to expand architecture autodetection for \`setupTermuxArch.sh\`.\n"
 	exit 1
 fi
 rm etc/resolv*
@@ -74,20 +66,11 @@ cat > $bin <<- EOM
 proot --link2symlink -0 -r ~/arch -b /dev/ -b /sys/ -b /proc/ -b /storage/ -b $HOME -w $HOME /bin/env -i HOME=/root TERM="$TERM" PS1='[termux@arch \W]\$ ' LANG=$LANG PATH=/bin:/usr/bin:/sbin:/usr/sbin /bin/bash --login
 EOM
 chmod 700 $bin
-echo 
-echo "Use \`pacman -Syu\` to update your Arch Linux in Termux distribution. Adjust your /etc/pacman.d/mirrorlist file in accordance with your 🌐 geographic location. See https://wiki.archlinux.org/index.php/Pacman for more information about easily managing packages in Arch Linux. Install packages with \`pacman -S package-name\`." 
-echo
-echo "👉 Use \`./arch/$bin\` from your \$HOME directory to launch Arch Linux in Termux for future sessions."
-echo
-echo "👉 This might be abbreviated to \`!.\` at the bash prompt after login into Termux."
-echo
-echo "Thank you for using \`setupTermuxArch.sh\` to install Arch Linux in Termux!"
-echo
+printf "\nUse \`pacman -Syu\` to update your Arch Linux in Termux distribution. Adjust your /etc/pacman.d/mirrorlist file in accordance with your 🌎 geographic location.\n"
+printf "\n👉 Use \`./arch/$bin\` from your \$HOME directory to launch Arch Linux in Termux for futuresessions.\n"
+printf "\n👉 This can be abbreviated to \`!.\` at the bash prompt after login into Termux.\n"
+printf "\nThank you for using \`setupTermuxArch.sh\` to install Arch Linux in Termux!\n"
 $HOME/arch/$bin
-echo
-echo "👉 Use \`./arch/$bin\` from your \$HOME directory to launch Arch Linux in Termux for future sessions."
-echo
-echo "👉 This might be abbreviated to \`!.\` at the bash prompt after login into Termux."
-echo
-echo "Thank you for using \`setupTermuxArch.sh\` to install Arch Linux in Termux!"
-echo
+printf "\n👉 Use \`./arch/$bin\` from your \$HOME directory to launch Arch Linux in Termux for future sessions.\n"
+printf "\n👉 This can be abbreviated to \`!.\` at the bash prompt after login into Termux.\n"
+printf "\nThank you for using \`setupTermuxArch.sh\` to install Arch Linux in Termux!"
