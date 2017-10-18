e15ab18c9c847a68cff06aef956fa979351c2092
S D Rausty <sdrausty@gmail.com>
2017-10-18 13:37:05 -0400

    Commit on 20171018 at 13:37:05 and at 1508348225 in Unix time.

--- setupTermuxArch.sh
+++ setupTermuxArch.sh
@@ -1,7 +1,7 @@
 #!/data/data/com.termux/files/usr/bin/sh -e
 # Copyright 2017 (c) all rights reserved \ 
 # by SDRausty https://sdrausty.github.io/termux-arch
-# Contributers @Soph1a7 et al.
+# Contributors @Soph1a7 et al.
 #####################################################################
 bin=startArch.sh
 mirror=mirror.archlinuxarm.org
@@ -28,7 +28,7 @@ if md5sum -c ArchLinuxARM-aarch64-latest.tar.gz.md5; then
 	proot --link2symlink tar -xf ArchLinuxARM-aarch64-latest.tar.gz 2>/dev/null||:
 else
 	echo
-	echo "ERROR md5sum missmatch! Remove $HOME/arch with all downloads and restart \`setupTermuxArch.sh\`."
+	echo "ERROR md5sum mismatch! Remove $HOME/arch with all downloads and restart \`setupTermuxArch.sh\`."
 	echo
 	echo "Alternatively, change \`mirror=$mirror\` in line 7 and remove $HOME/arch with all downloads, then restart \`setupTermuxArch.sh\`. Use https://archlinuxarm.org/about/mirrors to choose an available mirror in accordance with your 🌎 geographic location." 
 	echo
@@ -47,7 +47,7 @@ if md5sum -c ArchLinuxARM-armv7-latest.tar.gz.md5; then
 	proot --link2symlink tar -xf ArchLinuxARM-armv7-latest.tar.gz 2>/dev/null||:
 else
 	echo
-	echo "ERROR md5sum missmatch! Remove $HOME/arch with all downloads and restart \`setupTermuxArch.sh\`."
+	echo "ERROR md5sum mismatch! Remove $HOME/arch with all downloads and restart \`setupTermuxArch.sh\`."
 	echo
 	echo "Alternatively, change \`mirror=$mirror\` in line 7 and remove $HOME/arch with all downloads, then restart \`setupTermuxArch.sh\`. Use https://archlinuxarm.org/about/mirrors to choose an available mirror in accordance with your 🌎 geographic location." 
 	echo
@@ -60,7 +60,7 @@ else
 	echo
 	echo "👉 If you find a match, then please submit a pull request at https://github.com/sdrausty/TermuxArch/pulls with script modifications."
 	echo
-	echo "👉 Alternatively, submit a modification request at https://github.com/sdrausty/TermuxArch/issues if you find an architecture match. Please include output from \`uname -mo\` on the device in order to expand architecture autodetection for \`setupTermuxArch.sh\`."
+	echo "👉 Alternatively, submit a modification request at https://github.com/sdrausty/TermuxArch/issues if you find an architecture match. Please include output from \`uname -mo\` on the device in order to expand architecture auto-detection for \`setupTermuxArch.sh\`."
 	echo
 	exit 1
 fi
@@ -75,7 +75,7 @@ proot --link2symlink -0 -r ~/arch -b /dev/ -b /sys/ -b /proc/ -b /storage/ -b $H
 EOM
 chmod 700 $bin
 echo 
-echo "Use \`pacman -Syu\` to update your Arch Linux in Termux distribution. Adjust your /etc/pacman.d/mirrorlist file in accordance with your 🌎 geographic location." 
+echo "Use \`pacman -Syu\` to update your Arch Linux in Termux distribution. Adjust your /etc/pacman.d/mirrorlist file in accordance with your 🌎 geographic location. See https://wiki.archlinux.org/index.php/Pacman for more information about easily managing packages in Arch Linux. Install packages with \`pacman -S package-name\`." 
 echo
 echo "👉 Use \`./arch/$bin\` from your \$HOME directory to launch Arch Linux in Termux for future sessions."
 echo
