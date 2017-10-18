4f7a1b33839a50793e1d328c3faaaf0e54a2b1ed
S D Rausty <sdrausty@gmail.com>
2017-10-17 23:19:32 -0400

    Commit on 20171017 at 23:19:32 and at 1508296772 in Unix time.

--- setupTermuxArch.sh
+++ setupTermuxArch.sh
@@ -30,7 +30,7 @@ else
 	echo
 	echo "ERROR md5sum missmatch: Remove $HOME/arch with all downloads and restart \`setupTermuxArch.sh\`."
 	echo
-	echo "Alternatively, also change \`mirror=$mirror\` in line 7 and then restart \`setupTermuxArch.sh\`. Please use https://archlinuxarm.org/about/mirrors to choose an available mirror in accordance with your 🌎 geographic location." 
+	echo "Alternatively, change \`mirror=$mirror\` in line 7 and remove $HOME/arch with all downloads, then restart \`setupTermuxArch.sh\`. Please use https://archlinuxarm.org/about/mirrors to choose an available mirror in accordance with your 🌎 geographic location." 
 	exit 1
 fi
 elif [ "$(uname -m)" = "armv7l" ];then
@@ -47,7 +47,7 @@ else
 	echo
 	echo "ERROR md5sum missmatch: Remove $HOME/arch with all downloads and restart \`setupTermuxArch.sh\`."
 	echo
-	echo "Alternatively, also change \`mirror=$mirror\` in line 7 and then restart \`setupTermuxArch.sh\`. Please use https://archlinuxarm.org/about/mirrors to choose an available mirror in accordance with your 🌎 geographic location." 
+	echo "Alternatively, change \`mirror=$mirror\` in line 7 and remove $HOME/arch with all downloads, then restart \`setupTermuxArch.sh\`. Please use https://archlinuxarm.org/about/mirrors to choose an available mirror in accordance with your 🌎 geographic location." 
 	exit 1
 fi
 else
