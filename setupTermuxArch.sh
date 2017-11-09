52586c2d9cb6b3c3589f1fb5e9ee9696f3cf2d5d
S D Rausty <sdrausty@gmail.com>
2017-11-09 11:16:47 -0500

    Commit on 20171109 at 11:16:47 and at 1510244207 in Unix time.

--- setupTermuxArch.sh
+++ setupTermuxArch.sh
@@ -2,7 +2,7 @@
 # Website for this project at https://sdrausty.github.io/TermuxArch
 # See https://sdrausty.github.io/TermuxArch/CONTRIBUTORS Thank You! 
 # Copyright 2017 by SDRausty. All rights reserved.  🌎 🌍 🌏 🌐 🗺
-# If you are encountering issues regarding systemimage.tar.gz download or md5checksum, edit this script and change $mirror to your desired geographic location in knownconfigurations.sh.
+# If you are encountering issues with the system-image.tar.gz file regarding download time, repository website connection and/or md5 checksum error, edit this script and change $mirror to your desired geographic location in knownconfigurations.sh.  Before editing this file, ensure termux-wake-lock is running during script operation and that you have a stable Internet connection. 
 ################################################################################
 
 printf '\033]2;  Thank you for using `setupTermuxArch.sh` 📲 \007'"\n 🕛 \033[36;1m< 🕛 This setup script will attempt to set Arch Linux up in your Termux environment.  When successfully completed, you will be enjoying the bash prompt in Arch Linux in Termux on your smartphone or tablet.  \033[0mIf you do not see 🕐 one o'clock below after updating Termux and installing the required components for Arch Linux in Termux installation is completed, run this script again. You might want to check your Internet connection too.  \033[37;1mUpdating Termux and installing the required components for Arch Linux in Termux installation.  This will take some time.  \033[0m\n\n"
@@ -25,11 +25,12 @@ wget -q -N --show-progress https://raw.githubusercontent.com/sdrausty/TermuxArch
 }
 
 # Main Block
-termux-wake-lock
+termux-wake-lock # Before activating termux-wake-lock, setupTermuxArch.sh should check whether activation is necessary.
 depend 
 callsystem 
-$HOME/arch/$bin ||:
+$HOME/arch/$bin ||: # This call should be replaced with a PRoot function that completes setup from within Arch Linux similar to $bin without actually logging the user into the Arch Linux CLI (run a couple of commands, then complete setting up Arch Linux on device). 
 printtail
-termux-wake-unlock
+termux-wake-unlock # Ditto for deactivating termux-wake-lock with termux-wake-unlock, setupTermuxArch.sh should know whether deactivation is required.  termux-wake-unlock may become redundant if the following call is implemented.
+#kill -9 -1 The user should be informed that Termux will restart via running setupTermuxArch.sh before implementation of this call.  
 exit 
 
