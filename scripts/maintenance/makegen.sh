62d8a607dd9f2b8228ddadcbad45f9851a478d5a
S D Rausty <sdrausty@gmail.com>
2018-01-27 12:02:35 -0500

    Commit on 20180127 branch master 733898146

---
+++ scripts/maintenance/makegen.sh
@@ -0,0 +1,21 @@
+#!/bin/sh 
+# Copyright 2017-2018 by SDRausty. All rights reserved.
+# Website for this project at https://sdrausty.github.io/TermuxArch
+# See https://sdrausty.github.io/TermuxArch/CONTRIBUTORS Thank You 
+#############################################################################
+
+cdir=${PWD##*/}        
+cp setupTermuxArch.sh ..
+ls -al setupTermuxArch.sh 
+pwd
+md5sum *sh > termuxarchchecksum.md5 
+cd ..
+pwd
+bsdtar -czv -f setupTermuxArch.tar.gz --strip-components 3 $cdir/*
+rm $cdir/termuxarchchecksum.md5
+md5sum setupTermuxArch.tar.gz > setupTermuxArch.md5
+pwd
+ls -al setupTermuxArch.md5
+ls -al setupTermuxArch.sh 
+ls -al setupTermuxArch.tar.gz 
+
