c27b56791c5dee9e7644e907f532767193882ed1
S D Rausty <sdrausty@gmail.com>
2018-01-27 09:11:51 -0500

    Commit on 20180127 branch master 596262783

--- scripts/files/stable/necessaryfunctions.sh
+++ scripts/files/stable/necessaryfunctions.sh
@@ -224,7 +224,7 @@ sysinfo ()
 	if [ -d /storage/emulated/0/Download ]; then echo "/storage/emulated/0/Download exists"; else echo "/storage/emulated/0/Download not found"; fi >> setupTermuxArchdebug$ntime.log
 	printf "\nuname -mo results:\n\n" >> setupTermuxArchdebug$ntime.log
 	uname -mo >> setupTermuxArchdebug$ntime.log
-	printf "\nEnd setupTermuxArch debug information.\n\nPost this information along with information regarding your issue at https://github.com/sdrausty/TermuxArch/issues.  This debugging information is found in $(pwd)/$(ls setupTermuxArchdebug$ntime.log).  If you think screenshots will help in resolving this matter better, include them in your post please.  " >> setupTermuxArchdebug$ntime.log
+	printf "\nEnd setupTermuxArch debug information.\n\nPost this information along with information regarding your issue at https://github.com/sdrausty/TermuxArch/issues.  This debugging information is found in $(pwd)/$(ls setupTermuxArchdebug$ntime.log).  If you think screenshots will help in resolving this matter better, include them in your post please.  \n" >> setupTermuxArchdebug$ntime.log
 	cat setupTermuxArchdebug$ntime.log
 	printf "\n\033[0mSubmit this information if you plan to open up an issue at https://github.com/sdrausty/TermuxArch/issues to improve this installation script along with a screenshot of your topic.  \n"
 }
