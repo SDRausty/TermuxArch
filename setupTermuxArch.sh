5ecbd56aedd6a28b83cd29f9a3ecec37aae131e6
S D Rausty <sdrausty@gmail.com>
2018-01-27 05:57:52 -0500

    Commit on 20180127 branch master 149931337

--- setupTermuxArch.sh
+++ setupTermuxArch.sh
@@ -4,36 +4,13 @@
 # See https://sdrausty.github.io/TermuxArch/CONTRIBUTORS Thank You! 
 ################################################################################
 
-printoneoclock ()
-{
-	printf "If you do not see 🕐 one o'clock below, check your Internet connection and run this script again.  "
-}
-
-
 depends ()
 {
-	printf '\033]2;  Thank you for using `setupTermuxArch.sh` 📲 \007'"\n 🕛 \033[36;1m< 🕛 \033[1;34mThis setup script will attempt to set Arch Linux up in your Termux environment.  When successfully completed, you will be enjoying the bash prompt in Arch Linux in Termux on your smartphone or tablet. "
-	printoneoclock
-if [ -e $PREFIX/bin/bsdtar ] && [ -e $PREFIX/bin/proot ] && [ -e $PREFIX/bin/wget ] ; then
-	printf "Termux package requirements for Arch Linux: \033[36;1mOK  \n\n"
-else
-	printf "\n\n\033[36;1m"
-	apt-get -qq update && apt-get -qq upgrade -y
-	apt-get -qq install bsdtar proot wget --yes 
-fi
-if [ -e $PREFIX/bin/bsdtar ] && [ -e $PREFIX/bin/proot ] && [ -e $PREFIX/bin/wget ] ; then
-	:
-else
-	printf "\033[1;34m"  
-	printoneoclock
-	printf "\n\n\033[0m"
-	exit
-fi
-	printf "\n 🕧 < 🕛 \033[1;34mTermux package requirements for Arch Linux: \033[36;1mOK  \n\n"
+	installtermuxdepends
 	wget -q -N --show-progress https://raw.githubusercontent.com/sdrausty/TermuxArch/master/setupTermuxArch.tar.gz
 	wget -q -N --show-progress https://raw.githubusercontent.com/sdrausty/TermuxArch/master/setupTermuxArch.md5 
 	printf "\n"
-	if md5sum -c setupTermuxArch.md5 ; then
+if md5sum -c setupTermuxArch.md5 ; then
 	printf "\n 🕐 \033[36;1m< 🕛 \033[1;34mInstallation script download: \033[36;1mOK  \n\n\033[36;1m"
 	bsdtar -xf setupTermuxArch.tar.gz
 	rmds 
@@ -54,10 +31,37 @@ else
 fi
 }
 
+installtermuxdepends()
+{
+	printf '\033]2;  Thank you for using `setupTermuxArch.sh` 📲 \007'"\n 🕛 \033[36;1m< 🕛 \033[1;34mThis setup script will attempt to set Arch Linux up in your Termux environment.  When successfully completed, you will be enjoying the bash prompt in Arch Linux in Termux on your smartphone or tablet. "
+	printoneoclock
+if [ -e $PREFIX/bin/bsdtar ] && [ -e $PREFIX/bin/proot ] && [ -e $PREFIX/bin/wget ] ; then
+	printf "Termux package requirements for Arch Linux: \033[36;1mOK  \n\n"
+else
+	printf "\n\n\033[36;1m"
+	apt-get -qq update && apt-get -qq upgrade -y
+	apt-get -qq install bsdtar proot wget --yes 
+fi
+if [ -e $PREFIX/bin/bsdtar ] && [ -e $PREFIX/bin/proot ] && [ -e $PREFIX/bin/wget ] ; then
+	:
+else
+	printf "\033[1;34m"  
+	printoneoclock
+	printf "\n\n\033[0m"
+	exit
+fi
+	printf "\n 🕧 < 🕛 \033[1;34mTermux package requirements for Arch Linux: \033[36;1mOK  \n\n"
+}
+	
 printmd5syschkerror ()
 {
 	printf "\033[07;1m\033[31;1m\n 🔆 ERROR md5sum mismatch!  Setup initialization mismatch!\033[36;1m  Update your copy of setupTermuxArch.sh.  If you have updated it, this kind of error can go away, sort of like magic.  Waiting a few minutes before executing again is recommended, especially if you are using a new copy from https://raw.githubusercontent.com/sdrausty/TermuxArch/master/setupTermuxArch.sh on your system.  There are many reasons that generate checksum errors.  Proxies are one reason.  Mirroring and mirrors are another explanation for md5sum errors.  Either way this means,  \"Try again, initialization was not successful.\"  See https://sdrausty.github.io/TermuxArchPlus/md5sums for more information.  \n\n	Run setupTermuxArch.sh again. \033[31;1mExiting...  \n\033[0m"
 	exit 
+}
+
+printoneoclock ()
+{
+	printf "If you do not see 🕐 one o'clock below, check your Internet connection and run this script again.  "
 }
 
 printtail ()
