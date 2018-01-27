0092b35cbecf4e3e71a7712944f82d0832a7c458
sdrausty <sdrausty@gmail.com>
2018-01-27 01:09:55 -0500

    Commit on 20180127 branch master 116047205.

---
+++ scripts/maintenance/addSubmoduledfa.sh
@@ -0,0 +1,6 @@
+#!/bin/sh -e 
+# Copyright 2017 (c) all rights reserved 
+# by S D Rausty https://sdrausty.github.io
+# Update submodules to latest version. 
+#####################################################################
+git submodule add https://github.com/sdrausty/dfa scripts/frags/dfa
