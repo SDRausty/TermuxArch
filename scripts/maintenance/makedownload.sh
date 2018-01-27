13182ded6418d9be6c70dc7fc61bba922beb8b21
S D Rausty <sdrausty@gmail.com>
2018-01-27 10:20:36 -0500

    Commit on 20180127 branch master 658377980

--- scripts/maintenance/makedownload.sh
+++ scripts/maintenance/makedownload.sh
@@ -8,6 +8,7 @@ cdir=${PWD##*/}
 cp setupTermuxArch.sh ../../..
 md5sum *sh > termuxarchchecksum.md5 
 cd ../../..
-bsdtar -czv -f setupTermuxArch.tar.gz --strip-components 3 scripts/files/stable/*
+bsdtar -czv -f setupTermuxArch.tar.gz --strip-components 3 scripts/files/$cdir/*
 rm scripts/files/$cdir/termuxarchchecksum.md5
 md5sum setupTermuxArch.tar.gz > setupTermuxArch.md5
+
