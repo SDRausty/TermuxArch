069bf5d75b283114c0a479f08be1d179b1f9804e
S D Rausty <sdrausty@gmail.com>
2018-01-27 10:16:54 -0500

    Commit on 20180127 branch master 939668169

--- scripts/maintenance/makedownload.sh
+++ scripts/maintenance/makedownload.sh
@@ -1,20 +1,13 @@
 #!/bin/sh 
-# Copyright 2017 by SDRausty. All rights reserved.
+# Copyright 2017-2018 by SDRausty. All rights reserved.
 # Website for this project at https://sdrausty.github.io/TermuxArch
 # See https://sdrausty.github.io/TermuxArch/CONTRIBUTORS Thank You 
 #############################################################################
 
 cdir=${PWD##*/}        
 cp setupTermuxArch.sh ../../..
-ls -al setupTermuxArch.sh 
-pwd
 md5sum *sh > termuxarchchecksum.md5 
 cd ../../..
-pwd
 bsdtar -czv -f setupTermuxArch.tar.gz --strip-components 3 scripts/files/stable/*
 rm scripts/files/$cdir/termuxarchchecksum.md5
 md5sum setupTermuxArch.tar.gz > setupTermuxArch.md5
-pwd
-ls -al setupTermuxArch.md5
-ls -al setupTermuxArch.sh 
-ls -al setupTermuxArch.tar.gz 
