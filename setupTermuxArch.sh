2a48673765df7a8ab83a4b3550ad6f3439288a78
S D Rausty <sdrausty@gmail.com>
2018-01-27 19:43:54 -0500

    Commit on 20180127 branch master 713209830

--- setupTermuxArch.sh
+++ setupTermuxArch.sh
@@ -46,8 +46,8 @@ if [[ $dm = wget ]];then
 	wget -q -N --show-progress https://raw.githubusercontent.com/sdrausty/TermuxArch/master$dfl/setupTermuxArch.md5 
 	printf "\n"
 else
-	curl --fail --retry 4 -o setupTermuxArch.tar.gz https://raw.githubusercontent.com/sdrausty/TermuxArch/master$dfl/setupTermuxArch.tar.gz
-	curl --fail --retry 4 -o setupTermuxArch.md5 https://raw.githubusercontent.com/sdrausty/TermuxArch/master$dfl/setupTermuxArch.md5
+	curl --fail --retry 4 --verbose -o setupTermuxArch.tar.gz https://raw.githubusercontent.com/sdrausty/TermuxArch/master$dfl/setupTermuxArch.tar.gz
+	curl --fail --retry 4 --verbose -o setupTermuxArch.md5 https://raw.githubusercontent.com/sdrausty/TermuxArch/master$dfl/setupTermuxArch.md5
 	printf "\n"
 fi
 }
