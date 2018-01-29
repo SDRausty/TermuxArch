9f6c98401374817c051ab4232d03fc5347d2d96a
S D Rausty <sdrausty@gmail.com>
2018-01-29 03:36:23 -0500

    Commit on 20180129 branch master 882087205

--- scripts/files/experimental/necessaryfunctions.sh
+++ scripts/files/experimental/necessaryfunctions.sh
@@ -158,7 +158,7 @@ makesystem ()
 
 preproot ()
 {
-	if [ du ~/arch/*z -gt 1124345 ];then
+	if [ du ~/arch/*z -gt 11223344 ];then
 		if [ $(getprop ro.product.cpu.abi) = x86_64 ] || [ $(getprop ro.product.cpu.abi) = x86 ];then
 		proot --link2symlink -0 bsdtar -xpf $file --strip-components 1 ||:
 		else
