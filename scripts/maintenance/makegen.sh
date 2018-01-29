5c1ef144d5ab740de76c9db846b2f46bf8c83051
S D Rausty <sdrausty@gmail.com>
2018-01-28 20:09:12 -0500

    Commit on 20180128 branch master 711810617

--- scripts/maintenance/makegen.sh
+++ scripts/maintenance/makegen.sh
@@ -1,27 +1,31 @@
-#!/bin/sh 
+#!/bin/sh -e
 # Copyright 2017-2018 by SDRausty. All rights reserved.
 # Website for this project at https://sdrausty.github.io/TermuxArch
 # See https://sdrausty.github.io/TermuxArch/CONTRIBUTORS Thank You 
 #############################################################################
 
 cdir=${PWD##*/}        
+printf "This script will generate a checksum and tar.gz files in \$PROJECT/gen.\n\n"
 if [ ! -d "../../../gen" ] ; then
 mkdir -p ../../../gen
 fi
-cp setupTermuxArch.sh ..
-ls -al setupTermuxArch.sh 
 pwd
+ls -al se*
+printf "\n"
+cp setupTermuxArch.sh ..
 md5sum *sh > termuxarchchecksum.md5 
 cd ..
-pwd
 bsdtar -czv -f setupTermuxArch.tar.gz --strip-components 1 $cdir/*
-ls -al setupTermuxArch.tar.gz 
+pwd
+ls -al se*
+printf "Generated tar.gz file with internal md5 checksum.\n\n"
 rm $cdir/termuxarchchecksum.md5
 md5sum setupTermuxArch.tar.gz > setupTermuxArch.md5
-pwd
-ls -al setupTermuxArch.md5
-ls -al setupTermuxArch.sh 
-ls -al setupTermuxArch.tar.gz 
-mv set* ../../gen
+ls -al se*
+printf "Generated md5 checksum for tar.gz file.\n\n"
+mv se* ../../gen
 ls -al  ../../gen
-
+printf "Deposited entire project in \$PROJECT/gen.\n\n"
+printf  "$cdir\n\n"
+ls -al $cdir
+printf "\n\n"
