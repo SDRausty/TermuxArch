cb9ea270afbfae73c4c75d41d6a7d13216b4392c
S D Rausty <sdrausty@gmail.com>
2018-01-28 21:26:27 -0500

    Commit on 20180128 branch master 386107235

--- scripts/files/experimental/knownconfigurations.sh
+++ scripts/files/experimental/knownconfigurations.sh
@@ -9,7 +9,7 @@
 aarch64 ()
 {
 	file=ArchLinuxARM-aarch64-latest.tar.gz
-	mirror=os.archlinuxarm.org
+	mirror=il.us.mirror.archlinuxarm.org
 	path=/os/
 	makesystem 
 }
@@ -25,7 +25,7 @@ armv5l ()
 armv7lAndroid  ()
 {
 	file=ArchLinuxARM-armv7-latest.tar.gz 
-	mirror=os.archlinuxarm.org
+	mirror=il.us.mirror.archlinuxarm.org
 	path=/os/
 	makesystem 
 }
