6cbfc749acb4180941c10f0dc7ecd58f2c0efcaf
S D Rausty <sdrausty@gmail.com>
2018-01-29 08:50:31 -0500

    Commit on 20180129 branch master 971255848

--- scripts/files/stable/necessaryfunctions.sh
+++ scripts/files/stable/necessaryfunctions.sh
@@ -159,9 +159,9 @@ preproot ()
 {
 	if [ $(du ~/arch/*z | awk {'print $1}') -gt 112233 ];then
 		if [ $(getprop ro.product.cpu.abi) = x86_64 ] || [ $(getprop ro.product.cpu.abi) = x86 ];then
-			proot --link2symlink bsdtar -xpf $file --strip-components 1 2>/dev/null||:
+			proot --link2symlink -0 bsdtar -xpf $file --strip-components 1 2>/dev/null||:
 		else
-			proot --link2symlink bsdtar -xpf $file 2>/dev/null||:
+			proot --link2symlink -0 bsdtar -xpf $file 2>/dev/null||:
 		fi
 	else
 		printf "\n\nDownload Exception!  Exiting!\n\n"
