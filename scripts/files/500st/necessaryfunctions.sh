7ca144de4cbcfe8897d271bafd72a05d67761c1c
S D Rausty <sdrausty@gmail.com>
2018-02-03 05:04:56 -0500

    Commit on 20180203 branch master 193935948

--- scripts/files/500st/necessaryfunctions.sh
+++ scripts/files/500st/necessaryfunctions.sh
@@ -96,7 +96,7 @@ ftchstnd ()
 			wget -q -N --show-progress http://$mirror$path$file.md5 
 			wget -q -c --show-progress http://$mirror$path$file 
 		else
-			curl -q -L --fail --retry 4 -O http://$mirror$path$file.md5 -O http://$mirror$path$file
+			curl --verbose -L --fail --retry 4 -O http://$mirror$path$file.md5 -O http://$mirror$path$file
 		fi
 }
 
