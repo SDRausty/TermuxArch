7397f5f44783e2143f3d4b4a3eb107cbaa505227
S D Rausty <sdrausty@gmail.com>
2018-02-02 19:09:36 -0500

    Commit on 20180202 branch master 950998324

--- scripts/files/testn/setupTermuxArch.sh
+++ scripts/files/testn/setupTermuxArch.sh
@@ -153,7 +153,7 @@ rmds ()
 
 bin=startarch
 #dfl="/gen"
-dm=curl
+dm=wget
 
 if [[ $1 = [Cc][Pp]* ]] || [[ $1 = -[Cc][Pp]* ]] || [[ $1 = --[Cc][Pp]* ]] || [[ $1 = [Cc][Uu]* ]] || [[ $1 = -[Cc][Uu]* ]] || [[ $1 = --[Cc][Uu]* ]];then
 	dm=curl
