36cc75603fe2547c6d367752c006a1c637b12ade
S D Rausty <sdrausty@gmail.com>
2018-02-04 17:03:28 -0500

    Commit on 20180204 branch master 762205207

--- scripts/maintenance/makechangelog.sh
+++ scripts/maintenance/makechangelog.sh
@@ -3,7 +3,10 @@
 # Website for this project at https://sdrausty.github.io/TermuxArch
 # See https://sdrausty.github.io/TermuxArch/CONTRIBUTORS Thank You 
 #############################################################################
+cat CHANGE.log
 gsn.sh >> CHANGE.log
+cat CHANGE.log
 du -hs .git >> CHANGE.log
 du -hs >> CHANGE.log
+cat CHANGE.log
 #git push master --force 
