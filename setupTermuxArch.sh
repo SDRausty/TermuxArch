d78fe2bdd1c26eec9ba188788c827f2de05a44e2
S D Rausty <sdrausty@gmail.com>
2018-02-05 11:51:31 -0500

    Commit on 20180205 branch master 287205584

--- setupTermuxArch.sh
+++ setupTermuxArch.sh
@@ -17,7 +17,7 @@ chk ()
 		. systemmaintenance.sh
 		rmdsc 
 		printf "\n\033[36;1m 🕜 < 🕛 \033[1;34mTermuxArch integrity: \033[36;1mOK: "
-		printf "Running v0.4.993101278"
+		printf "Running v0.4.287205584"
 		printf "\n\033[0m"
 	else
 		rmdsc 
@@ -144,7 +144,7 @@ rmds ()
 
 args=$@
 bin=startarch
-dfl="/gen"
+#dfl="/gen"
 dm=wget
 pvsz=$(du -b setupTermuxArch.sh)
 #
