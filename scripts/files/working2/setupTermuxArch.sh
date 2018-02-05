ad0590294bea0194c7b641145559235a97c3b76a
S D Rausty <sdrausty@gmail.com>
2018-02-05 10:13:52 -0500

    Commit on 20180205 branch master 550728913

--- scripts/files/working2/setupTermuxArch.sh
+++ scripts/files/working2/setupTermuxArch.sh
@@ -17,7 +17,7 @@ chk ()
 		. systemmaintenance.sh
 		rmdsc 
 		printf "\n\033[36;1m 🕜 < 🕛 \033[1;34mTermuxArch integrity: \033[36;1mOK: "
-		printf "Running v0.4.907814158"
+		printf "Running v0.4.550728913"
 		printf "\n\033[0m"
 	else
 		rmdsc 
