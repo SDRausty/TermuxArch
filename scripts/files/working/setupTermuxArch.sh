45099c26376f70a5dafccfe5e56f05c25ea17ef2
S D Rausty <sdrausty@gmail.com>
2018-02-05 12:39:33 -0500

    Commit on 20180205 branch master 122486516

--- scripts/files/working/setupTermuxArch.sh
+++ scripts/files/working/setupTermuxArch.sh
@@ -17,7 +17,7 @@ chk ()
 		. systemmaintenance.sh
 		rmdsc 
 		printf "\n\033[36;1m 🕜 < 🕛 \033[1;34mTermuxArch integrity: \033[36;1mOK: "
-		printf "Running v0.5.634369749"
+		printf "Running v0.5.122486516"
 		printf "\n\033[0m"
 	else
 		rmdsc 
--- setupTermuxArch.sh
+++ setupTermuxArch.sh
@@ -9,7 +9,7 @@ chk ()
 {
 	if md5sum -c termuxarchchecksum.md5 ; then
 		chkself 
-		. archsystemconfigs.sh
+		ldconf
 		. getimagefunctions.sh
 		. knownconfigurations.sh
 		. necessaryfunctions.sh
@@ -17,7 +17,7 @@ chk ()
 		. systemmaintenance.sh
 		rmdsc 
 		printf "\n\033[36;1m 🕜 < 🕛 \033[1;34mTermuxArch integrity: \033[36;1mOK: "
-		printf "Running v0.5.634369739"
+		printf "Running v0.5.122486516"
 		printf "\n\033[0m"
 	else
 		rmdsc 
@@ -55,7 +55,6 @@ depends ()
 	dwnl
 	chkdwn
 	chk
-	ldconf
 }
 
 dwnl ()
@@ -74,6 +73,8 @@ ldconf ()
 {
 	if [ -f "myTermuxArchConfigs.sh" ];then
 		. myTermuxArchConfigs.sh
+	else
+		. archsystemconfigs.sh
 	fi
 }
 
