1736dddc969dc348c591ff8f53d3edb0fe12e045
S D Rausty <sdrausty@gmail.com>
2018-02-05 08:12:53 -0500

    Commit on 20180205 branch master 326708713

--- scripts/files/working/getimagefunctions.sh
+++ scripts/files/working/getimagefunctions.sh
@@ -18,7 +18,7 @@ adjustmd5file ()
 		rm md5sums.txt
 	else
 		if [[ $dm = wget ]];then 
-			wget -q -N --show-progress http://$mirror$path$file.md5 
+			wget -N --show-progress http://$mirror$path$file.md5 
 		else
 			curl -q --fail --retry 4 -O -L http://$mirror$path$file.md5
 		fi
@@ -65,7 +65,7 @@ detectsystem2p ()
 ftchstnd ()
 {
 		if [[ $dm = wget ]];then 
-			wget -q -N --show-progress http://$mirror$path$file.md5 
+			wget -N --show-progress http://$mirror$path$file.md5 
 			wget -q -c --show-progress http://$mirror$path$file 
 		else
 			curl -q -L --fail --retry 4 -O http://$mirror$path$file.md5 -O http://$mirror$path$file
