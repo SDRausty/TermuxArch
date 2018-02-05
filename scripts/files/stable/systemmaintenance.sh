1005259ba15f07a7731690d32a35974141a4323d
S D Rausty <sdrausty@gmail.com>
2018-02-05 05:22:12 -0500

    Commit on 20180205 branch master 136591370

--- scripts/files/stable/systemmaintenance.sh
+++ scripts/files/stable/systemmaintenance.sh
@@ -25,8 +25,8 @@ sysinfo ()
 	printf "\ngetprop ro.product.device result:\n\n" >> setupTermuxArchdebug$ntime.log
 	getprop ro.product.device >> setupTermuxArchdebug$ntime.log
 	printf "\nDownload directory information results.\n\n" >> setupTermuxArchdebug$ntime.log
-	ls -al ~/storage/downloads  2>>setupTermuxArchdebug$ntime.log >> setupTermuxArchdebug$ntime.log $dgfl
-	ls -al ~/downloads 2>>setupTermuxArchdebug$ntime.log  >> setupTermuxArchdebug$ntime.log $dgfl
+	ls -al ~/storage/downloads  2>>setupTermuxArchdebug$ntime.log >> setupTermuxArchdebug$ntime.log $dgfl ||:
+	ls -al ~/downloads 2>>setupTermuxArchdebug$ntime.log  >> setupTermuxArchdebug$ntime.log $dgfl ||:
 	if [ -d /sdcard/Download ]; then echo "/sdcard/Download exists"; else echo "/sdcard/Download not found"; fi >> setupTermuxArchdebug$ntime.log 
 	if [ -d /storage/emulated/0/Download ]; then echo "/storage/emulated/0/Download exists"; else echo "/storage/emulated/0/Download not found"; fi >> setupTermuxArchdebug$ntime.log
 	printf "\nuname -mo results:\n\n" >> setupTermuxArchdebug$ntime.log
@@ -53,8 +53,8 @@ rmarch ()
        	fi
 	if [ -d $HOME/arch ] ;then
 		cd $HOME/arch
-		rm -rf * $dgfl
-		find -type d -exec chmod 700 {} \; $dgfl
+		rm -rf * $dgfl ||:
+		find -type d -exec chmod 700 {} \; $dgfl ||:
 		cd ..
 		rm -rf $HOME/arch
 	else 
