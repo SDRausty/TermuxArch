c02a688a03b8b1a73103e79e44fbd68b3f1615ca
S D Rausty <sdrausty@gmail.com>
2018-02-07 19:50:17 -0500

    Commit on 20180207 branch master 510042396

--- gen/setupTermuxArch.sh
+++ gen/setupTermuxArch.sh
@@ -40,7 +40,7 @@ chkdwn ()
 chkself ()
 {
 	crs=$(<setupTermuxArch.sh) 
-	if ! diff -q $pvs $crs &>/dev/null; then
+	if diff -q $pvs $crs &>/dev/null; then
 		:
 	else
 		printf "\nsetupTermuxArch.sh: UPDATED\nTermuxArch: RESTARTED\n\033[0m"
@@ -174,4 +174,4 @@ elif [[ $1 = "" ]] || [[ $1 = [Ii]* ]] || [[ $1 = -[Ii]* ]] || [[ $1 = --[Ii]* ]
 else
 	printusage
 fi
-echo TermuxArch v0.5.563667882
+echo TermuxArch v0.5.273306877
--- scripts/files/working/setupTermuxArch.sh
+++ scripts/files/working/setupTermuxArch.sh
@@ -40,7 +40,7 @@ chkdwn ()
 chkself ()
 {
 	crs=$(<setupTermuxArch.sh) 
-	if ! diff -q $pvs $crs &>/dev/null; then
+	if diff -q $pvs $crs &>/dev/null; then
 		:
 	else
 		printf "\nsetupTermuxArch.sh: UPDATED\nTermuxArch: RESTARTED\n\033[0m"
