0a8d85bbdb3a5f57a6ae1d6463d5662cbf1461ae
S D Rausty <sdrausty@gmail.com>
2018-02-08 15:42:29 -0500

    Commit on 20180208 branch master 196489604

--- scripts/files/stable/printoutstatements.sh
+++ scripts/files/stable/printoutstatements.sh
@@ -16,7 +16,7 @@ printdetectedsystem ()
 
 printdownloading ()
 {
-	printf "\n\033[36;1m 🕒 < 🕛 \033[1;34mActivated termux-wake-lock.  Downloading \033[36;1m$file \033[1;34mand the corresponding checksum.  \033[37;1mPending Internet connection, this might take a long while.  \033[36;1mTo resolve possible 404 error see: \`bash setupTermuxArch.sh --help\`\n\n"'\033]2;  🕒 < 🕛 Downloading the system image file and the corresponding checksum.  \007'
+	printf "\n\033[36;1m 🕒 < 🕛 \033[1;34mActivated termux-wake-lock.  Downloading \033[36;1m$file \033[1;34mand the corresponding checksum.  \033[37;1mPending Internet connection, this might take a long while.  \033[36;1mSee \`bash setupTermuxArch.sh --help\` to resolve possible 404 error.\n\n"'\033]2;  🕒 < 🕛 Downloading the system image file and the corresponding checksum.  \007'
 }
 
 printconfigq ()
@@ -26,7 +26,7 @@ printconfigq ()
 
 printmd5check ()
 {
-	printf "\n\033[36;1m 🕠 < 🕛 \033[1;34mChecking download integrity with md5sum.  \033[37;1mThis may take a little while.  \033[36;1mTo resolve possible md5sum error see: \`bash setupTermuxArch.sh --help\`\n\n"
+	printf "\n\033[36;1m 🕠 < 🕛 \033[1;34mChecking download integrity with md5sum.  \033[37;1mThis may take a little while.  \033[36;1mSee \`bash setupTermuxArch.sh --help\` to resolve possible md5sum error.\n\n"
 }
 
 printmd5error ()
