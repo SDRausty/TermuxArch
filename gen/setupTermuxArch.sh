6aabf31b69aaeaf5ddb38f6328bac8016968b415
S D Rausty <sdrausty@gmail.com>
2018-02-07 20:51:53 -0500

    Commit on 20180207 branch master 647957444

--- gen/setupTermuxArch.sh
+++ gen/setupTermuxArch.sh
@@ -17,7 +17,7 @@ chk ()
 		. systemmaintenance.sh
 		rmdsc 
 		printf "\n\033[36;1m 🕑 < 🕛 \033[1;34mTermuxArch integrity: \033[36;1mOK: "
-		printf "Running v0.5.5 id159408578"
+		printf "Running v0.5.5 id149408578"
 		printf "\n\033[0m"
 	else
 		rmdsc 
@@ -40,7 +40,7 @@ chkdwn ()
 chkself ()
 {
 	crs=$(<setupTermuxArch.sh) 
-	if ! diff -q $pvs $crs &>/dev/null; then
+	if ! diff -q pvs crs &>/dev/null ; then
 		echo did nothing
 		echo did nothing
 		echo did nothing
@@ -54,7 +54,7 @@ chkself ()
 depends ()
 {
 	printf '\033]2;  Thank you for using `setupTermuxArch.sh` 📲 \007'"\n 🕛 \033[36;1m< 🕛 \033[1;34mTermuxArch will attempt to install Linux in Termux.  Arch Linux will be available upon successful completion.  If you do not see one o'clock 🕐 below, check wireless connection.  Ensure background data is not restricted.  "
-	printf "Running v0.5 id159408578"
+	printf "Running v0.5 id149408578"
 	printf "\n"
 	predepends 
 	dwnl
@@ -169,4 +169,4 @@ elif [[ $1 = "" ]] || [[ $1 = [Ii]* ]] || [[ $1 = -[Ii]* ]] || [[ $1 = --[Ii]* ]
 else
 	printusage
 fi
-echo TermuxArch v0.5.154642271
+echo TermuxArch v0.5.460957030
