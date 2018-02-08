09af4d0f24eeb3399482cbbbe6b960f1d57233dd
S D Rausty <sdrausty@gmail.com>
2018-02-08 14:39:05 -0500

    Commit on 20180208 branch master 530755118

--- scripts/files/stable/printoutstatements.sh
+++ scripts/files/stable/printoutstatements.sh
@@ -16,7 +16,7 @@ printdetectedsystem ()
 
 printdownloading ()
 {
-	printf "\n\033[36;1m 🕒 < 🕛 \033[1;34mActivating termux-wake-lock.  Downloading \033[36;1m$file \033[1;34mand the corresponding checksum.  \033[37;1mPending Internet connection, this might take a long while.  \n\n\033[36;1m"'\033]2;  🕒 < 🕛 Downloading the system image file and the corresponding checksum.  \007'
+	printf "\n\033[36;1m 🕒 < 🕛 \033[1;34mActivated termux-wake-lock.  Downloading \033[36;1m$file \033[1;34mand the corresponding checksum.  \033[37;1mPending Internet connection, this might take a long while.  \n\n\033[36;1m"'\033]2;  🕒 < 🕛 Downloading the system image file and the corresponding checksum.  \007'
 }
 
 printconfigq ()
