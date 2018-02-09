49d12d223f84091f0817a16b01b8fac632e1282c
S D Rausty <sdrausty@gmail.com>
2018-02-09 13:34:20 -0500

    Commit on 20180209 branch master 674823298

--- scripts/files/working/getimagefunctions.sh
+++ scripts/files/working/getimagefunctions.sh
@@ -27,12 +27,7 @@ adjustmd5file ()
 
 ftchstnd ()
 {
-		if [[ $dm = wget ]];then 
-			wget -q -N --show-progress http://$mirror$path$file.md5 
-			wget -q -c --show-progress http://$mirror$path$file 
-		else
-			curl -q -L --fail --retry 4 -O http://$mirror$path$file.md5 -O http://$mirror$path$file
-		fi
+		curl -q -L --fail --retry 4 -O http://$mirror$path$file.md5 -O http://$mirror$path$file
 }
 
 getimage ()
