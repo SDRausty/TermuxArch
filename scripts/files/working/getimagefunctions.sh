ff401c55f0bc7e36eb786c7c5843dece6e4ea0e5
S D Rausty <sdrausty@gmail.com>
2018-02-10 01:19:18 -0500

    Commit on 20180210 branch master 645239558

--- scripts/files/working/getimagefunctions.sh
+++ scripts/files/working/getimagefunctions.sh
@@ -34,7 +34,12 @@ ftchit ()
 	if [[ $dm = wget ]];then 
 		printf "Defaulting to \`curl\` for system image download.  "
 		printdownloadingftchit 
+		echo $mirror
+		echo $path
 		curl -q --fail --retry 4 -O -L http://$mirror$path$file.md5 -O http://$nmirror$path$file
+		echo 104
+		curl -q --fail --retry 4 -O http://$mirror$path$file.md5 -O http://$nmirror$path$file
+
 	else
 		printdownloadingftchit 
 		curl -q --fail --retry 4 -O -L http://$mirror$path$file.md5 -O http://$nmirror$path$file
