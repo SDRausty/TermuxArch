32ffa922d13b6a4f3603fe62c0327548db109751
S D Rausty <sdrausty@gmail.com>
2018-02-14 00:36:25 -0500

    Commit on 20180214 branch master 829935351

--- scripts/files/stable/setupTermuxArch.sh
+++ scripts/files/stable/setupTermuxArch.sh
@@ -50,7 +50,7 @@ chk ()
 			rm setupTermuxArch.tmp
 		fi
 		printf "\n\033[36;1m 🕑 < 🕛 \033[1;34mTermuxArch "
-		printf "v0.8 id143644984"
+		printf "v0.8 id829935351"
 		printf " integrity: \033[36;1mOK\n\033[1;30m"
 	else
 		rmdsc 
@@ -204,7 +204,7 @@ printtail ()
 printusage ()
 {
 	printf "\n\n\033[1;34mUsage information for \033[1;32msetupTermuxArch.sh \033[1;34m"
-		printf "v0.8 id143644984"
+		printf "v0.8 id829935351"
 	printf ".  Arguments can abbreviated to one letter; Two letter arguments are acceptable.  For example, \033[1;32mbash setupTermuxArch.sh cs\033[1;34m will use \033[1;32mcurl\033[1;34m to download TermuxArch and produce the \033[1;32msetupTermuxArchdebug.log\033[1;34m file.\n\n\033[1;33mDEBUG\033[1;34m    Use \033[1;32msetupTermuxArch.sh --sysinfo \033[1;34mto create \033[1;32msetupTermuxArchdebug.log\033[1;34m and populate it with debug information.  Post this information along with detailed information about the issue at https://github.com/sdrausty/TermuxArch/issues.  If screenshots will help in resolving the issue better, include them in a post along with information from the debug log file.\n\n\033[1;33mHELP\033[1;34m     Use \033[1;32msetupTermuxArch.sh --help \033[1;34mto output this help screen.\n\n\033[1;33mINSTALL\033[1;34m  Run \033[1;32m./setupTermuxArch.sh\033[1;34m without arguments in a bash shell to install Arch Linux in Termux.  Use \033[1;32mbash setupTermuxArch.sh --curl \033[1;34mto envoke \033[1;32mcurl\033[1;34m as the download manager.  Copy \033[1;32mknownconfigurations.sh\033[1;34m to \033[1;32msetupTermuxArchConfigs.sh\033[1;34m with preferred mirror.  After editing \033[1;32msetupTermuxArchConfigs.sh\033[1;34m, run \033[1;32mbash setupTermuxArch.sh\033[1;34m and \033[1;32msetupTermuxArchConfigs.sh\033[1;34m loads automatically from the same directory.  Change mirror to desired geographic location to resolve download errors.\n\n\033[1;33mPURGE\033[1;34m    Use \033[1;32msetupTermuxArch.sh --uninstall\033[1;34m \033[1;34mto uninstall Arch Linux from Termux.\n"
 }
 
@@ -247,7 +247,7 @@ rmarchc ()
 rmarchq ()
 {
 	if [ -d $HOME/arch ] ;then
-		printf "\n\033[33;1mTermuxArch: DIRECTORY WARNING!  \`$HOME/arch/\` directory detected.  \033[1;30mTermux Arch installation will continue.  \033[33;1mInstalling into a clean directory is recommended.  \033[1;30mUninstalling before continuing is suggested.\n"
+		printf "\n\033[33mTermuxArch: DIRECTORY WARNING!  \`$HOME/arch/\` directory detected.  \033[1;30mTermux Arch installation will continue.  \033[33mInstalling into a clean directory is recommended.  \033[1;30mUninstalling before continuing is suggested.\n"
 		rmarch
 	fi
 }
@@ -277,7 +277,7 @@ rmbloom ()
 rmbloomq ()
 {
 	if [ -d $HOME/TermuxArchBloom ] ;then
-		printf "\n\n\033[33;1mTermuxArch: DIRECTORY WARNING!  \`$HOME/TermuxArchBloom/\` directory detected.  \033[1;30mTermux Arch will continue.\n"
+		printf "\n\n\033[33mTermuxArch: DIRECTORY WARNING!  \`$HOME/TermuxArchBloom/\` directory detected.  \033[1;30mTermux Arch will continue.\n"
 		rmbloom
 	fi
 }
@@ -306,13 +306,13 @@ spaceinfo ()
 		usspace="${usrspace: : -1}"
 		if [ $(getprop ro.product.cpu.abi) = arm64-v8a ];then
 			if [[ "$usspace" < "1.5" ]];then
-				spaceMessage="\n\033[1;33mTermuxArch: FREE SPACE WARNING!  \033[36mStart thinking about cleaning out some stuff.  \033[1;33m$usrspace of free user space is available on this device.  \033[1;36mThe recommended minimum to install Arch Linux in Termux PRoot for aarch64 is 1.5G of free user space.\n\033[0m"
+				spaceMessage="\n\033[33mTermuxArch: FREE SPACE WARNING!  \033[36mStart thinking about cleaning out some stuff.  \033[33m$usrspace of free user space is available on this device.  \033[1;36mThe recommended minimum to install Arch Linux in Termux PRoot for aarch64 is 1.5G of free user space.\n\033[0m"
 			else
 				spaceMessage=""
 			fi
 		elif [ $(getprop ro.product.cpu.abi) = armeabi-v7a ];then
 			if [[ "$usspace" < "1.25" ]];then
-				spaceMessage="\n\033[1;33mTermuxArch: FREE SPACE WARNING!  \033[36mStart thinking about cleaning out some stuff.  \033[1;33m$usrspace of free user space is available on this device.  \033[1;36mThe recommended minimum to install Arch Linux in Termux PRoot for armv7 is 1.25G of free user space.\n\033[0m"
+				spaceMessage="\n\033[33mTermuxArch: FREE SPACE WARNING!  \033[36mStart thinking about cleaning out some stuff.  \033[33m$usrspace of free user space is available on this device.  \033[1;36mThe recommended minimum to install Arch Linux in Termux PRoot for armv7 is 1.25G of free user space.\n\033[0m"
 			else
 				spaceMessage=""
 			fi
@@ -320,7 +320,7 @@ spaceinfo ()
 		spaceMessage=""
 		fi
 	else
-		spaceMessage="\n\033[1;33mTermuxArch: FREE SPACE WARNING!  \033[36mStart thinking about cleaning out some stuff.  \033[1;33m$usrspace of free user space is available on this device.  \033[1;36mThe recommended minimum to install Arch Linux in Termux PRoot is more than 1.5G for aarch64, more than 1.25G for armv7 and more than 1G of free user space for all other architectures.\n\033[0m"
+		spaceMessage="\n\033[33mTermuxArch: FREE SPACE WARNING!  \033[36mStart thinking about cleaning out some stuff.  \033[33m$usrspace of free user space is available on this device.  \033[1;36mThe recommended minimum to install Arch Linux in Termux PRoot is more than 1.5G for aarch64, more than 1.25G for armv7 and more than 1G of free user space for all other architectures.\n\033[0m"
 	fi
 	printf "$spaceMessage"
 }
