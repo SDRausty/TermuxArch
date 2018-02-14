ca3f3c874aecbba2f0171c4a20bc4e29a40c073f
S D Rausty <sdrausty@gmail.com>
2018-02-14 08:36:46 -0500

    Commit on 20180214 branch master 710406930

--- scripts/files/working/setupTermuxArch.sh
+++ scripts/files/working/setupTermuxArch.sh
@@ -381,7 +381,7 @@ bin=startarch
 dm=wget
 dmverbose="-q"
 #dmverbose="-v"
-versionid="v0.8 id409675484"
+versionid="v0.8 id710406930"
 
 
 if [[ $1 = [Cc][Dd]* ]] || [[ $1 = -[Cc][Dd]* ]] || [[ $1 = --[Cc][Dd]* ]] || [[ $1 = [Cc][Ss]* ]] || [[ $1 = -[Cc][Ss]* ]] || [[ $1 = --[Cc][Ss]* ]];then
