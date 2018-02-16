57981ca1a5f422f49158123af09cb9f315e6058f
S D Rausty <sdrausty@gmail.com>
2018-02-16 17:32:27 -0500

    Commit on 20180216 branch master 402367212

---
+++ scripts/files/stable/v0.8.1.id502434699/getimagefunctions.sh
@@ -0,0 +1,60 @@
+#!/bin/bash -e
+# Copyright 2017-2018 by SDRausty. All rights reserved.  🌎 🌍 🌏 🌐 🗺
+# Hosting https://sdrausty.github.io/TermuxArch courtesy https://pages.github.com
+# https://sdrausty.github.io/TermuxArch/CONTRIBUTORS Thank you for your help.  
+# https://sdrausty.github.io/TermuxArch/README has information about this project. 
+################################################################################
+
+ftchit ()
+{
+	if [[ $dm = wget ]];then 
+		printdownloadingftchit 
+		wget $dmverbose -N --show-progress http://$mirror$path$file.md5 
+		wget $dmverbose -c --show-progress http://$mirror$path$file 
+	else
+		printdownloadingftchit 
+		curl $dmverbose -C - --fail --retry 4 -O http://$mirror$path$file.md5 -O http://$mirror$path$file 
+	fi
+}
+
+ftchstnd ()
+{
+	#cmirror="http://mirror.archlinuxarm.org/"
+	cmirror="http://os.archlinuxarm.org/"
+	if [[ $dm = wget ]];then 
+		printf "\033[0;32mContacting mirror \033[1;32m$cmirror.  "
+		curl -v $cmirror 2>gmirror
+		nmirror=$(grep Location gmirror | awk {'print $3'}) 
+		rm gmirror
+		printdownloadingftch 
+		wget $dmverbose -N --show-progress $nmirror$path$file.md5 
+		wget $dmverbose -N --show-progress $nmirror$path$file 
+	else
+		printf "\033[0;32mContacting mirror \033[1;32m$cmirror.  "
+		curl -v $cmirror 2>gmirror
+		nmirror=$(grep Location gmirror | awk {'print $3'}) 
+		rm gmirror
+		printdownloadingftch 
+		curl $dmverbose --fail --retry 4 -O $nmirror$path$file.md5 -O $nmirror$path$file
+	fi
+}
+
+getimage ()
+{
+	if [[ $dm = wget ]];then 
+		printdownloadingx86 
+		wget $dmverbose -N --show-progress http://$mirror${path}md5sums.txt
+		file=$(grep boot md5sums.txt | awk {'print $2'})
+		sed '2q;d' md5sums.txt > $file.md5
+		rm md5sums.txt
+		wget $dmverbose -c --show-progress http://$mirror$path$file 
+	else
+		printdownloadingx86 
+		curl $dmverbose -C - --fail --retry 4 -O http://$mirror${path}md5sums.txt
+		file=$(grep boot md5sums.txt | awk {'print $2'})
+		sed '2q;d' md5sums.txt > $file.md5
+		rm md5sums.txt
+		curl $dmverbose -C - --fail --retry 4 -O http://$mirror$path$file 
+	fi
+}
+
