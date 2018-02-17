40a54af460555057c47526155e26f6dcd5d1dddb
S D Rausty <sdrausty@gmail.com>
2018-02-17 00:35:01 -0500

    Commit on 20180217 branch master 498540527

--- scripts/files/working/getimagefunctions.sh
+++ scripts/files/working/getimagefunctions.sh
@@ -44,19 +44,27 @@ getimage ()
 	if [[ $dm = wget ]];then 
 		printdownloadingx86 
 		wget $dmverbose -N --show-progress http://$mirror${path}md5sums.txt
-		file=$(grep boot md5sums.txt | awk {'print $2'})
+		if [ $(getprop ro.product.cpu.abi) = x86 ];then
+			file=$(grep i686 md5sums.txt | awk {'print $2'})
+		else
+			file=$(grep boot md5sums.txt | awk {'print $2'})
+		fi
 		sed '2q;d' md5sums.txt > $file.md5
 		rm md5sums.txt
 		printdownloadingx86two 
 		wget $dmverbose -c --show-progress http://$mirror$path$file 
 	else
 		printdownloadingx86 
-		curl $dmverbose -C - --fail --retry 4 -O http://$mirror${path}md5sums.txt
-		file=$(grep boot md5sums.txt | awk {'print $2'})
+		curl $dmverbose --fail --retry 4 -OL http://$mirror${path}md5sums.txt
+		if [ $(getprop ro.product.cpu.abi) = x86 ];then
+			file=$(grep i686 md5sums.txt | awk {'print $2'})
+		else
+			file=$(grep boot md5sums.txt | awk {'print $2'})
+		fi
 		sed '2q;d' md5sums.txt > $file.md5
 		rm md5sums.txt
 		printdownloadingx86two 
-		curl $dmverbose -C - --fail --retry 4 -O http://$mirror$path$file 
+		curl $dmverbose -C - --fail --retry 4 -OL http://$mirror$path$file 
 	fi
 }
 
