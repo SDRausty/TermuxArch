b8038786182d87808774b65ed0e8dde04932144c
S D Rausty <sdrausty@gmail.com>
2018-02-17 20:30:36 -0500

    Commit on 20180217 branch master 426572623

--- setupTermuxArch.sh
+++ setupTermuxArch.sh
@@ -413,7 +413,7 @@ dm=curl
 dmverbose=""
 #dmverbose="-v"
 ntime=`date +%N`
-versionid="v0.8.3 "
+versionid="v0.8.3"
 
 if [[ $1 = [Cc][Dd]* ]] || [[ $1 = -[Cc][Dd]* ]] || [[ $1 = --[Cc][Dd]* ]] || [[ $1 = [Cc][Ss]* ]] || [[ $1 = -[Cc][Ss]* ]] || [[ $1 = --[Cc][Ss]* ]];then
 	dm=curl
