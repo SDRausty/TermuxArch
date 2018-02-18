c0473ecad95f6d05d58d2febd95ba29f706a8e1b
S D Rausty <sdrausty@gmail.com>
2018-02-18 00:17:18 -0500

    Commit on 20180218 branch master 305145024

--- gen/setupTermuxArch.sh
+++ gen/setupTermuxArch.sh
@@ -408,12 +408,12 @@ spaceinfoq ()
 args=$@
 bin=startarch
 dfl=/gen
-dm=curl
-#dm=wget
+#dm=curl
+dm=wget
 dmverbose="-q"
 #dmverbose="-v"
 ntime=`date +%N`
-versionid="gen.v0.8.3 id234407334"
+versionid="gen.v0.8.3 id305145024"
 
 if [[ $1 = [Cc][Dd]* ]] || [[ $1 = -[Cc][Dd]* ]] || [[ $1 = --[Cc][Dd]* ]] || [[ $1 = [Cc][Ss]* ]] || [[ $1 = -[Cc][Ss]* ]] || [[ $1 = --[Cc][Ss]* ]];then
 	dm=curl
