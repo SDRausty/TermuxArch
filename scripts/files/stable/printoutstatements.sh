a5b9e39c4cf5c8529e74005034b24da6167cb6e9
S D Rausty <sdrausty@gmail.com>
2018-02-19 08:00:45 -0500

    Commit on 20180219 branch master 743387460

--- scripts/files/stable/printoutstatements.sh
+++ scripts/files/stable/printoutstatements.sh
@@ -60,7 +60,7 @@ printmd5error ()
 
 printmd5success ()
 {
-	printf '\033]2;  🕛 > 🕡 Uncompressing the image file.  This will take a long time; Be patient.\007'"\n\033[1;34m 🕛 > 🕕 \033[1;34mSystem image file download integrity: \033[32;1mOK\n\n\033[1;34m 🕛 > 🕡 \033[1;34mUncompressing \033[32;1m$file\033[1;34m.  \033[1;37mThis will take a long time; Be patient.\n\033[0m"
+	printf '\033]2;  🕛 > 🕡 Uncompressing the image file.  This will take a long time; Be patient.\007'"\033[1;34m 🕛 > 🕕 \033[1;34mSystem image file download integrity: \033[32;1mOK\n\n\033[1;34m 🕛 > 🕡 \033[1;34mUncompressing \033[32;1m$file\033[1;34m.  \033[1;37mThis will take a long time; Be patient.\n\033[0m"
 }
 
 printmismatch ()
