77d69582918e980661c00466adaaad220b265720
S D Rausty <sdrausty@gmail.com>
2018-02-24 06:37:06 -0500

    Commit on 20180224 branch master 088648794

--- scripts/files/working/setupTermuxArch.sh
+++ scripts/files/working/setupTermuxArch.sh
@@ -213,7 +213,7 @@ ifnano ()
 	if [ ! -e $PREFIX/bin/nano ];then
 		printf "\n\033[1;34mInstalling \033[0;32mnano\033[1;34m…\n\n\033[1;32m"
 		pkg install nano --yes 
-		printf "\n\033[1;34mInstalling \033[0;32mnano\033[1;34m: \033[1;32mDONE\n\033[0m"
+		printf "\n\033[1;34mInstalling \033[0;32mnano\033[1;34m: \033[1;32mDONE\n\n\033[0m"
 	fi
 	if [ ! -e $PREFIX/bin/nano ];then
 		pe
