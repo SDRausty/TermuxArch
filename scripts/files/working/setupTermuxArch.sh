2c6d1282188d84778610334ab6c34696fe9975ba
S D Rausty <sdrausty@gmail.com>
2018-03-01 12:44:39 -0500

    Commit on 20180301 branch master 830985639

--- scripts/files/working/setupTermuxArch.sh
+++ scripts/files/working/setupTermuxArch.sh
@@ -653,13 +653,13 @@ wgetif ()
 
 args=$@
 bin=startarch
-dfl=/gen
+#dfl=/gen
 #dm=curl
 #dm=wget
 dmverbose="-q"
 #dmverbose="-v"
 stime=`date +%s|grep -o '....$'`
-versionid="gen.v0.8.10 id546144419"
+versionid="v0.9"
 
 setrootdir 
 
