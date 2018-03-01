a5ec0d766eaf342d214dcc4ffe5b3eb70216f643
S D Rausty <sdrausty@gmail.com>
2018-03-01 11:22:10 -0500

    Commit on 20180301 branch master 950057009

--- scripts/files/stable/archsystemconfigs.sh
+++ scripts/files/stable/archsystemconfigs.sh
@@ -20,16 +20,20 @@ addauserps ()
 {
 	# add default Arch Termux user 
 	cat > root/bin/addauserps <<- EOM
+	#!/bin/bash -e
+	useradd \$1
+	cp -r /root /home/\$1
+	su - \$1
 	\cat > $HOME/bin/startarch\$1 <<- EOM
 	#!/bin/bash -e
 	unset LD_PRELOAD
 	exec proot --link2symlink -0 -r $HOME$rootdir/ -b /dev/ -b /sys/ -b /proc/ -b /storage/ -b $HOME -w $HOME /bin/env -i HOME=/root TERM="$TERM" PS1='[termux@arch \W]\$ ' LANG=$LANG PATH=/bin:/usr/bin:/sbin:/usr/sbin /bin/su - \$1 --login
-	\EOM
-	useradd \$1
-	cp -r /root /home/\$1
-	su - \$1
 	EOM
-	chmod 770 root/bin/addauser 
+	echo EOM >> root/bin/addauserps 
+	cat >> root/bin/addauserps <<- EOM
+	chmod 770 $HOME/bin/startarch\$1 >> root/bin/addauserps 
+	EOM
+	chmod 770 root/bin/addauserps 
 }
 
 addbash_profile ()
