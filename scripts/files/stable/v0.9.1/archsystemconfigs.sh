51bfd29c14da1334365c7fa7b6bc4fc01fe1ada8
S D Rausty <sdrausty@gmail.com>
2018-03-02 04:11:36 -0500

    Commit on 20180302 branch master 493787127

---
+++ scripts/files/stable/v0.9.1/archsystemconfigs.sh
@@ -0,0 +1,240 @@
+#!/bin/bash -e
+# Copyright 2017-2018 by SDRausty. All rights reserved.  🌎 🌍 🌏 🌐 🗺
+# Hosting https://sdrausty.github.io/TermuxArch courtesy https://pages.github.com
+# https://sdrausty.github.io/TermuxArch/CONTRIBUTORS Thank you for your help.  
+# https://sdrausty.github.io/TermuxArch/README has information about this project. 
+################################################################################
+
+addauser ()
+{
+	# Add Arch Linux user.
+	cat > root/bin/addauser <<- EOM
+	useradd \$1
+	cp -r /root /home/\$1
+	su - \$1
+	EOM
+	chmod 770 root/bin/addauser 
+}
+
+addauserps ()
+{
+	# Add Arch Linux user and create user login Termux startup script. 
+	cat > root/bin/addauserps <<- EOM
+	#!/bin/bash -e
+	useradd \$1
+	cp -r /root /home/\$1
+	su - \$1
+	EOM
+	echo "cat > $HOME/bin/startarchuser\$1 <<- EOM " >> root/bin/addauserps 
+	cat >> root/bin/addauserps <<- EOM
+	#!/bin/bash -e
+	unset LD_PRELOAD
+	exec proot --link2symlink -0 -r $HOME$rootdir/ -b /dev/ -b /sys/ -b /proc/ -b /storage/ -b $HOME -w $HOME /bin/env -i HOME=/root TERM="$TERM" PS1='[termux@arch \W]\$ ' LANG=$LANG PATH=/bin:/usr/bin:/sbin:/usr/sbin /bin/su - \$1 --login
+	EOM
+	echo EOM >> root/bin/addauserps 
+	cat >> root/bin/addauserps <<- EOM
+	chmod 770 $HOME/bin/startarchuser\$1
+	EOM
+	chmod 770 root/bin/addauserps 
+}
+
+addbash_profile ()
+{
+	cat > root/.bash_profile <<- EOM
+	PATH=\$HOME/bin:\$PATH
+	. \$HOME/.bashrc
+	PS1="[\A\[\033[0;32m\] \W \[\033[0m\]]\\$ "
+	EOM
+	if [ -e $HOME/.bash_profile ] ; then
+		grep proxy $HOME/.bash_profile |grep "export" >>  root/.bash_profile 2>/dev/null||:
+	fi
+}
+
+addbashrc ()
+{
+	cat > root/.bashrc <<- EOM
+	alias c='cd .. && pwd'
+	alias ..="cd ../.. && pwd"
+	alias ...="cd ../../.. && pwd"
+	alias ....="cd ../../../.. && pwd"
+	alias .....="cd ../../../../.. && pwd"
+	alias d='du -hs'
+	alias e='logout'
+	alias g='ga; gcm; gp'
+	alias gca='git commit -a'
+	alias gcam='git commit -am'
+	#alias gp='git push https://username:password@github.com/username/repository.git master'
+	alias h='history >> \$HOME/.historyfile'
+	alias j='jobs'
+	alias l='ls -alG'
+	alias ls='ls --color=always'
+	alias p='pwd'
+	alias q='logout'
+	alias rf='rm -rf'
+	. /etc/motd
+	EOM
+	if [ -e $HOME/.bashrc ] ; then
+		grep proxy $HOME/.bashrc |grep "export" >>  root/.bashrc 2>/dev/null||:
+	fi
+}
+
+adddfa ()
+{
+	cat > root/bin/dfa <<- EOM
+	#!/bin/bash -e
+	units=\`df 2>/dev/null | awk 'FNR == 1 {print \$2}'\`
+	usrspace=\`df 2>/dev/null | grep "/data" | awk {'print \$4'}\`
+	printf "\033[0;33m\$usrspace \$units of free user space is available on this device.\n\033[0m"
+	EOM
+	chmod 770 root/bin/dfa 
+}
+
+addprofile ()
+{
+	cat > root/.profile <<- EOM
+	. \$HOME/.bash_profile
+	EOM
+	if [ -e $HOME/.profile ] ; then
+		grep "proxy" $HOME/.profile |grep "export" >>  root/.profile 2>/dev/null||:
+	fi
+}
+
+addga ()
+{
+	cat > root/bin/ga  <<- EOM
+	#!/bin/bash -e
+	if [ ! -e /usr/bin/git ] ; then
+		pacman -Syu git --noconfirm
+		git add .
+	else
+		git add .
+	fi
+	EOM
+	chmod 770 root/bin/ga 
+}
+
+addgcl ()
+{
+	cat > root/bin/gcl  <<- EOM
+	#!/bin/bash -e
+	if [ ! -e /usr/bin/git ] ; then
+		pacman -Syu git --noconfirm
+		git clone \$@
+	else
+		git clone \$@
+	fi
+	EOM
+	chmod 770 root/bin/gcl 
+}
+
+addgcm ()
+{
+	cat > root/bin/gcm  <<- EOM
+	#!/bin/bash -e
+	if [ ! -e /usr/bin/git ] ; then
+		pacman -Syu git --noconfirm
+		git commit
+	else
+		git commit
+	fi
+	EOM
+	chmod 770 root/bin/gcm 
+}
+
+addgpl ()
+{
+	cat > root/bin/gpl  <<- EOM
+	#!/bin/bash -e
+	if [ ! -e /usr/bin/git ] ; then
+		pacman -Syu git --noconfirm
+		git pull
+	else
+		git pull
+	fi
+	EOM
+	chmod 770 root/bin/gpl 
+}
+
+addgp ()
+{
+	cat > root/bin/gp  <<- EOM
+	#!/bin/bash -e
+	#git push https://username:password@github.com/username/repository.git master
+	if [ ! -e /usr/bin/git ] ; then
+		pacman -Syu git --noconfirm
+		git push
+	else
+		git push
+	fi
+	EOM
+	chmod 700 root/bin/gp 
+}
+
+addmotd ()
+{
+	cat > etc/motd  <<- EOM
+	printf "\033[1;34mWelcome to Arch Linux in Termux!  Enjoy!\033[0m\033[1;34m
+	
+	Chat:    \033[0m\033[mhttps://gitter.im/termux/termux/\033[0m\033[1;34m
+	Help:    \033[0m\033[34minfo <query> \033[0m\033[mand \033[0m\033[34mman <query> \033[0m\033[1;34m
+	Portal:  \033[0m\033[mhttps://wiki.termux.com/wiki/Community\033[0m\033[1;34m
+	
+	Install a package: \033[0m\033[34mpacman -S <package>\033[0m\033[1;34m
+	More  information: \033[0m\033[34mpacman [-D|F|Q|R|S|T|U] --help\033[0m\033[1;34m
+	Search   packages: \033[0m\033[34mpacman -Ss <query>\033[0m\033[1;34m
+	Upgrade  packages: \033[0m\033[34mpacman -Syu \n\033[0m"
+	EOM
+}
+
+addresolvconf ()
+{
+	rm etc/resolv* 2>/dev/null||:
+	cat > etc/resolv.conf <<- EOM
+	nameserver 8.8.8.8
+	nameserver 8.8.4.4
+	EOM
+}
+
+addt ()
+{
+	cat > root/bin/t  <<- EOM
+	#!/bin/bash -e
+	if [ ! -e /usr/bin/tree ] ; then
+		pacman -Syu tree --noconfirm
+		tree \$@
+	else
+		tree \$@
+	fi
+	EOM
+	chmod 770 root/bin/t 
+}
+
+addv ()
+{
+	cat > root/bin/v  <<- EOM
+	#!/bin/bash -e
+	if [ ! -e /usr/bin/vim ] ; then
+		pacman -Syu vim --noconfirm
+		vim \$@
+	else
+		vim \$@
+	fi
+	EOM
+	chmod 770 root/bin/v 
+}
+
+addyt ()
+{
+	cat > root/bin/yt  <<- EOM
+	#!/bin/bash -e
+	if [ ! -e /usr/bin/youtube-dl ] ; then
+		pacman -Syu python-pip --noconfirm
+		pip install youtube-dl
+		youtube-dl \$@
+	else
+		youtube-dl \$@
+	fi
+	EOM
+	chmod 770 root/bin/yt 
+}
+
---
+++ scripts/files/stable/v0.9.1/necessaryfunctions.sh
@@ -0,0 +1,256 @@
+#!/bin/bash -e
+# Copyright 2017-2018 by SDRausty. All rights reserved.  🌎 🌍 🌏 🌐 🗺
+# Hosting https://sdrausty.github.io/TermuxArch courtesy https://pages.github.com
+# https://sdrausty.github.io/TermuxArch/CONTRIBUTORS Thank you for your help.  
+# https://sdrausty.github.io/TermuxArch/README has information about this project. 
+################################################################################
+
+callsystem ()
+{
+	mkdir -p $HOME$rootdir
+	cd $HOME$rootdir
+	detectsystem
+}
+
+copybin2path ()
+{
+	if [[ ":$PATH:" == *":$HOME/bin:"* ]] && [ -d $HOME/bin ]; then
+		BPATH=$HOME/bin
+	else
+		BPATH=$PREFIX/bin
+	fi
+	while true; do
+	printf "\033[0;34m 🕛 > 🕚 \033[0mCopy \033[1m$bin\033[0m to \033[1m$BPATH\033[0m?  " 
+	read -p "Answer yes or no [Y|n] " answer
+	if [[ $answer = [Yy]* ]] || [[ $answer = "" ]];then
+		cp $HOME$rootdir/$bin $BPATH
+		printf "\n\033[0;34m 🕛 > 🕦 \033[0mCopied \033[1m$bin\033[0m to \033[1m$BPATH\033[0m.\n\n"
+		break
+	elif [[ $answer = [Nn]* ]] || [[ $answer = [Qq]* ]];then
+		printf "\n"
+		break
+	else
+		printf "\n\033[0;34m 🕛 > 🕚 \033[0mYou answered \033[33;1m$answer\033[0m.\n\n\033[0;34m 🕛 > 🕚 \033[0mAnswer yes or no [Y|n]\n\n"
+	fi
+	done
+}
+
+detectsystem ()
+{
+	printdetectedsystem
+	if [ $(getprop ro.product.cpu.abi) = armeabi ];then
+		armv5l
+	elif [ $(getprop ro.product.cpu.abi) = armeabi-v7a ];then
+		detectsystem2 
+	elif [ $(getprop ro.product.cpu.abi) = arm64-v8a ];then
+		aarch64
+	elif [ $(getprop ro.product.cpu.abi) = x86 ];then
+		i686 
+	elif [ $(getprop ro.product.cpu.abi) = x86_64 ];then
+		x86_64
+	else
+		printmismatch 
+	fi
+}
+
+detectsystem2 ()
+{
+	if [[ $(getprop ro.product.device) == *_cheets ]];then
+		armv7lChrome 
+	else
+		armv7lAndroid  
+	fi
+}
+
+mainblock ()
+{ 
+	rmarchq
+	spaceinfoq
+	callsystem 
+	termux-wake-unlock
+#	rm $HOME$rootdir/root/bin/setupbin.sh
+	printfooter
+	$HOME$rootdir/$bin 
+}
+
+makebin ()
+{
+	makestartbin 
+	printconfigq 
+	touchupsys 
+}
+
+makefinishsetup ()
+{
+	binfs=finishsetup.sh  
+	cat > root/bin/$binfs <<- EOM
+	#!/bin/bash -e
+	EOM
+	if [ -e $HOME/.bash_profile ]; then
+		grep "proxy" $HOME/.bash_profile | grep "export" >>  root/bin/$binfs 2>/dev/null ||:
+	fi
+	if [ -e $HOME/.bashrc ]; then
+		grep "proxy" $HOME/.bashrc  | grep "export" >>  root/bin/$binfs 2>/dev/null ||:
+	fi
+	if [ -e $HOME/.profile ]; then
+		grep "proxy" $HOME/.profile | grep "export" >>  root/bin/$binfs 2>/dev/null ||:
+	fi
+	cat >> root/bin/$binfs <<- EOM
+	printf "\n"
+	if [ $(getprop ro.product.cpu.abi) = x86 ] || [ $(getprop ro.product.cpu.abi) = x86_64 ];then
+		pacman -Syu sed --noconfirm ||:
+	else
+		pacman -Syu --noconfirm ||:
+	fi
+	printf "\n"
+	locale-gen ||:
+	printf "\n"
+	tzselect
+	printf "\r"
+	printf "\r"
+	printf '\033]2; 🕛 > 🕙 Arch Linux in Termux is installed and configured.  📲  \007'
+	EOM
+	chmod 770 root/bin/finishsetup.sh 
+}
+
+makesetupbin ()
+{
+	cat > root/bin/setupbin.sh <<- EOM
+	#!$PREFIX/bin/bash -e
+	unset LD_PRELOAD
+	exec proot --link2symlink -0 -r $HOME$rootdir/ -b /dev/ -b /sys/ -b /proc/ -b /storage/ -b $HOME -w $HOME /bin/env -i HOME=/root TERM="$TERM" PS1='[termux@arch \W]\$ ' LANG=$LANG PATH=/bin:/usr/bin:/sbin:/usr/sbin $HOME$rootdir/root/bin/finishsetup.sh
+	EOM
+	chmod 700 root/bin/setupbin.sh
+}
+
+makestartbin ()
+{
+	cat > $bin <<- EOM
+	#!$PREFIX/bin/bash -e
+	unset LD_PRELOAD
+	exec proot --link2symlink -0 -r $HOME$rootdir/ -b /dev/ -b /sys/ -b /proc/ -b /storage/ -b $HOME -w $HOME /bin/env -i HOME=/root TERM="$TERM" PS1='[termux@arch \W]\$ ' LANG=$LANG PATH=/bin:/usr/bin:/sbin:/usr/sbin /bin/bash --login
+	EOM
+	chmod 700 $bin
+}
+
+makesystem ()
+{
+	printwla 
+	termux-wake-lock 
+	printdone 
+	if [ $(getprop ro.product.cpu.abi) = x86 ] || [ $(getprop ro.product.cpu.abi) = x86_64 ];then
+		getimage
+	else
+		if [ "$mirror" = "os.archlinuxarm.org" ] || [ "$mirror" = "mirror.archlinuxarm.org" ]; then
+			ftchstnd 
+		else
+			ftchit
+		fi
+	fi
+	printmd5check
+	if md5sum -c $file.md5 1>/dev/null ; then
+		printmd5success
+		preproot 
+	else
+		rmarchrm 
+		printmd5error
+	fi
+	rm *.tar.gz *.tar.gz.md5
+	makebin 
+}
+
+preproot ()
+{
+	if [ $(du $HOME$rootdir/*z | awk {'print $1'}) -gt 112233 ];then
+		if [ $(getprop ro.product.cpu.abi) = x86 ] || [ $(getprop ro.product.cpu.abi) = x86_64 ];then
+			#cd $HOME
+			#proot --link2symlink -0 $PREFIX/bin/applets/tar xf $HOME$rootdir$file 
+			#cd $HOME$rootdir
+			proot --link2symlink -0 bsdtar -xpf $file --strip-components 1 
+		else
+			proot --link2symlink -0 $PREFIX/bin/applets/tar xf $file 
+		fi
+	else
+		printf "\n\n\033[1;31mDownload Exception!  Execute \033[0;32mbash setupTermuxArch.sh\033[1;31m again…\n"'\033]2;  Thank you for using setupTermuxArch.sh.  Execute `bash setupTermuxArch.sh` again…\007'
+		exit
+	fi
+}
+
+runfinishsetup ()
+{
+	if [[ $ed = "" ]];then
+		editors 
+	fi
+	sed -i '1i# TermuxArch vi instructions:\n# Use the hjkl keys to navigate. <h down j up k l>\n# Numbers are multipliers.\n# 15j then i will open insert mode in vi for the Geo-IP mirror.\n# Enter # to comment out this line.\n# Use ESC to return to command mode.\n# Long tap KEYBOARD in the side pane if you do not see ESC.\n#After locating a local mirror, use x to delete # uncommenting the mirror.\n# Choose only one mirror.  Use :x to save and finish your work.' $HOME$rootdir/etc/pacman.d/mirrorlist
+	$ed $HOME$rootdir/etc/pacman.d/mirrorlist
+	while true; do
+		printf "\n\033[0;32mWould you like to run \033[1;32mlocale-gen\033[0;32m to generate the en_US.UTF-8 locale, or edit \033[1;32m/etc/locale.gen\033[0;32m specifying your preferred language(s) before running \033[1;32mlocale-gen\033[0;32m?  "
+		read -p "Answer yes to run [Y|e] " ye
+	if [[ $ye = [Yy]* ]] || [[ $ye = "" ]];then
+		break
+	elif [[ $ye = [Ee]* ]] || [[ $ye = [Nn]* ]];then
+		$ed $HOME$rootdir/etc/locale.gen
+		break
+	else
+		printf "\nYou answered \033[1;36m$ye\033[1;32m.\n"
+		printf "\nAnswer yes to run [Y|e]\n"
+	fi
+	done
+	$HOME$rootdir/root/bin/setupbin.sh 
+}
+
+runfinishsetupq ()
+{
+	while true; do
+		printf "\n\033[0;32mWould you like to run \033[1;32mfinishsetup.sh\033[0;32m to complete the Arch Linux configuration and update now, or at a later time?  \033[1;32mNow is recommended.  \033[0;32m"
+		read -p "Answer yes to complete the Arch Linux configuration and update now; Or answer no for later [Y|n] " nl
+	if [[ $nl = [Yy]* ]] || [[ $nl = "" ]];then
+		runfinishsetup 
+		break
+	elif [[ $nl = [Nn]* ]];then
+		printf "\n\033[0;32mSet the geographically nearby mirror in \033[1;32m/etc/pacman.d/mirrorlist\033[0;32m first.  Then use \033[1;32m$HOME$rootdir/root/bin/setupbin.sh\033[0;32m in Termux to run \033[1;32mfinishsetup.sh\033[0;32m or simply \033[1;32mfinishsetup.sh\033[0;32m in Arch Linux Termux PRoot to complete the Arch Linux configuration and update."
+		break
+	else
+		printf "\nYou answered \033[1;36m$nl\033[1;32m.\n"
+		printf "\nAnswer yes to complete, or no for later [Y|n]\n"
+	fi
+	done
+	printf "\n"
+}
+
+setlocalegen()
+{
+	if [ -e etc/locale.gen ]; then
+		sed -i '/\#en_US.UTF-8 UTF-8/{s/#//g;s/@/-at-/g;}' etc/locale.gen 
+	else
+		cat >  etc/locale.gen <<- EOM
+		en_US.UTF-8 UTF-8 
+		EOM
+	fi
+}
+
+touchupsys ()
+{
+	mkdir -p root/bin
+	addauser
+	addauserps
+	addbash_profile 
+	addbashrc 
+	adddfa
+	addga
+	addgcl
+	addgcm
+	addgp
+	addgpl
+	addmotd
+	addprofile 
+	addresolvconf 
+	addt 
+	addyt 
+	addv 
+	setlocalegen
+	makefinishsetup
+	makesetupbin 
+	runfinishsetupq
+}
+
