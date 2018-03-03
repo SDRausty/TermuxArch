bad8bc6a39619d5bf709a9552ae666e06ba8aed2
S D Rausty <sdrausty@gmail.com>
2018-03-02 20:13:16 -0500

    Commit on 20180302 branch master 628292726

--- gen/setupTermuxArch.sh
+++ gen/setupTermuxArch.sh
@@ -659,7 +659,7 @@ dfl=/gen
 dmverbose="-q"
 #dmverbose="-v"
 stime=`date +%s|grep -o '....$'`
-versionid="gen.v0.9.1 id820389744"
+versionid="gen.v0.9.1 id628292726"
 
 setrootdir 
 
--- scripts/files/working/necessaryfunctions.sh
+++ scripts/files/working/necessaryfunctions.sh
@@ -87,9 +87,9 @@ lkernid ()
 {
 	ur="$($PREFIX/bin/applets/uname -r)"
 	declare -g kid=0
-	declare -i KERNEL_VERSION=`echo $ur |awk -F'.' '{print $1}'`
-	declare -i MAJOR_REVISION=`echo $ur |awk -F'.' '{print $2}'`
-	declare -i MINOR_REVISION=`echo $ur |awk -F'.' '{print $3}' | awk -F'-' '{print $1}'`
+	declare -i KERNEL_VERSION=$(echo $ur |awk -F'.' '{print $1}')
+	declare -i MAJOR_REVISION=$(echo $ur |awk -F'.' '{print $2}')
+	declare -i MINOR_REVISION="$(echo $ur |awk -F'.' '{print $3}' | awk -F'-' '{print $1}')"
 	if [ "$KERNEL_VERSION" -le 2 ]; then
 		kid=1
 	else
