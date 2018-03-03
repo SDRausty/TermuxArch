78aaef0e3e786d168cf77e55c01d1f38756286c2
S D Rausty <sdrausty@gmail.com>
2018-03-03 16:29:18 -0500

    Commit on 20180303 branch master 119368218

--- gen/setupTermuxArch.sh
+++ gen/setupTermuxArch.sh
@@ -656,7 +656,7 @@ dfl=/gen
 dmverbose="-q"
 #dmverbose="-v"
 stime=`date +%s|grep -o '....$'`
-versionid="gen.v0.9.2 id042815285"
+versionid="gen.v0.9.2 id119368218"
 
 setrootdir 
 
--- scripts/files/working/necessaryfunctions.sh
+++ scripts/files/working/necessaryfunctions.sh
@@ -123,13 +123,20 @@ makefinishsetup ()
 	cat >> root/bin/$binfs <<- EOM
 	printf "\n"
 	if [ $(getprop ro.product.cpu.abi) = x86 ] || [ $(getprop ro.product.cpu.abi) = x86_64 ];then
-		pacman-key --init
 		echo disable-scdaemon > /etc/pacman.d/gnupg/gpg-agent.conf
+		pacman-key --init
+		if [ $(getprop ro.product.cpu.abi) = x86 ];then
+			pacman -S archlinux32-keyring --noconfirm ||:
+		else
+			pacman -S archlinux-keyring --noconfirm ||:
+		fi
+		pacman-key --populate
 		pacman -Syu sed --noconfirm ||:
 	else
-		pacman-key --init
 		echo disable-scdaemon > /etc/pacman.d/gnupg/gpg-agent.conf
-		pacman -Syu --noconfirm ||:
+		pacman-key --init
+		pacman-key --populate
+		pacman -Syu archlinux-keyring --noconfirm ||:
 	fi
 	printf "\n"
 	locale-gen ||:
