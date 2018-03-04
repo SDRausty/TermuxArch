4a557df87517934901e19243fe968e296f3f55ad
S D Rausty <sdrausty@gmail.com>
2018-03-03 23:29:50 -0500

    Commit on 20180303 branch master 228891172

--- scripts/files/working/archsystemconfigs.sh
+++ scripts/files/working/archsystemconfigs.sh
@@ -219,11 +219,11 @@ addkeys ()
 	echo 0
 	rm -rf /etc/pacman.d/gnupg
 	echo 0
-	pacman -S gnupg --noconfirm 
-	echo 0
 	pacman-key --init 
 	echo 0
 	echo disable-scdaemon > /etc/pacman.d/gnupg/gpg-agent.conf 
+	echo 0
+	pacman-key --populate archlinux 
 	echo 0
 	if [ $(getprop ro.product.cpu.abi) = x86 ] || [ $(getprop ro.product.cpu.abi) = x86_64 ];then
 		if [ $(getprop ro.product.cpu.abi) = x86 ];then
@@ -234,10 +234,6 @@ addkeys ()
 	else
 		pacman -S archlinux-keyring --noconfirm 
 	fi
-	echo 0
-	pacman-key --populate archlinux 
-	echo 0
-	termux-wake-unlock
 	echo 0
 	EOM
 	chmod 700 root/bin/addkeys
--- scripts/files/working/setupTermuxArch.sh
+++ scripts/files/working/setupTermuxArch.sh
@@ -656,7 +656,7 @@ dfl=/gen
 dmverbose="-q"
 #dmverbose="-v"
 stime=`date +%s|grep -o '....$'`
-versionid="gen.v0.9.2 id734931934"
+versionid="gen.v0.9.2 id228891172"
 
 setrootdir 
 
