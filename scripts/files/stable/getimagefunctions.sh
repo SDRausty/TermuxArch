439d8845fe34c62a7cbec11e38e545318cb364d1
S D Rausty <sdrausty@gmail.com>
2018-03-05 16:15:22 -0500

    Commit on 20180305 branch master 215884597

--- scripts/files/stable/getimagefunctions.sh
+++ scripts/files/stable/getimagefunctions.sh
@@ -26,14 +26,14 @@ ftchstnd ()
 		printdone 
 		printdownloadingftch 
 		wget $dmverbose -N --show-progress $nmirror$path$file.md5 
-		wget $dmverbose -N --show-progress $nmirror$path$file 
+		wget $dmverbose -c --show-progress $nmirror$path$file 
 	else
 		curl -v $cmirror 2>gmirror
 		nmirror=$(grep Location gmirror | awk {'print $3'}) 
 		rm gmirror
 		printdone 
 		printdownloadingftch 
-		curl $dmverbose --fail --retry 4 -O $nmirror$path$file.md5 -O $nmirror$path$file
+		curl $dmverbose -C - --fail --retry 4 -O $nmirror$path$file.md5 -O $nmirror$path$file
 	fi
 }
 
@@ -52,7 +52,7 @@ getimage ()
 		printdownloadingx86two 
 		wget $dmverbose -c --show-progress http://$mirror$path$file 
 	else
-		curl $dmverbose --fail --retry 4 -OL http://$mirror${path}md5sums.txt
+		curl $dmverbose --fail --retry 4 -O http://$mirror${path}md5sums.txt
 		if [ $(getprop ro.product.cpu.abi) = x86 ];then
 			file=$(grep i686 md5sums.txt | awk {'print $2'})
 		else
@@ -61,7 +61,7 @@ getimage ()
 		sed '2q;d' md5sums.txt > $file.md5
 		rm md5sums.txt
 		printdownloadingx86two 
-		curl $dmverbose -C - --fail --retry 4 -OL http://$mirror$path$file 
+		curl $dmverbose -C - --fail --retry 4 -O http://$mirror$path$file 
 	fi
 }
 
