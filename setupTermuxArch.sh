eec0510b75daa2117d07aa6e9a62b45b72ec7a1d
S D Rausty <sdrausty@gmail.com>
2018-03-08 00:17:35 -0500

    Commit on 20180308 branch master 382005290

--- setupTermuxArch.sh
+++ setupTermuxArch.sh
@@ -251,7 +251,7 @@ edq2 ()
 
 intro ()
 {
-	rmarchq
+#	rmarchq
 	rootdirexception 
 	spaceinfoq
 	printf "\n\033[0;34m 🕛 > 🕛 \033[1;34msetupTermuxArch $versionid will attempt to install Linux in \033[0;32m$HOME$rootdir\033[1;34m.  Arch Linux will be available upon successful completion.  Ensure background data is not restricted.  Check the wireless connection if you do not see one o'clock 🕐 below.  "
