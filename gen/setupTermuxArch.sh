11a054916566eff342a6234350d7f8a589bda29c
S D Rausty <sdrausty@gmail.com>
2018-03-14 07:06:25 -0400

    Commit on 20180314 branch master 618061108

--- gen/setupTermuxArch.sh
+++ gen/setupTermuxArch.sh
@@ -358,7 +358,8 @@ namestartarch ()
 		aarch=""
 		startbin2=arch
 	else
-		aarch=$(echo '$darch' | sed 's/[^a-zA-Z0-9]//g')
+#		aarch=$(echo '$darch' | sed 's/[^a-zA-Z0-9]//g')
+		aarch=$darch
 		startbin2=archin
 	fi
 	bin=start$startbin2$aarch
@@ -676,7 +677,7 @@ dfl=/gen
 dmverbose="-q"
 #dmverbose="-v"
 stime=`date +%s|grep -o '....$'`
-versionid="gen.v1.3 id454170196"
+versionid="gen.v1.3 id618061108"
 
 # [curl debug|curl sysinfo] Get device system information using `curl`.
 if [[ $1 = [Cc][Dd]* ]] || [[ $1 = -[Cc][Dd]* ]] || [[ $1 = --[Cc][Dd]* ]] || [[ $1 = [Cc][Ss]* ]] || [[ $1 = -[Cc][Ss]* ]] || [[ $1 = --[Cc][Ss]* ]];then
