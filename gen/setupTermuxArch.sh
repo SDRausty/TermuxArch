e3e8e7ff092a4859f551eda8ce41109e83fa982a
S D Rausty <sdrausty@gmail.com>
2018-03-17 07:59:38 -0400

    Commit on 20180317 branch master 477353968

--- gen/setupTermuxArch.sh
+++ gen/setupTermuxArch.sh
@@ -687,7 +687,7 @@ dfl=/gen
 dmverbose="-q"
 #dmverbose="-v"
 stime=`date +%s|grep -o '....$'`
-versionid="gen.v1.3 id552390051"
+versionid="gen.v1.3 id477353968"
 
 # [curl debug|curl sysinfo] Get device system information using `curl`.
 if [[ $1 = [Cc][Dd]* ]] || [[ $1 = -[Cc][Dd]* ]] || [[ $1 = --[Cc][Dd]* ]] || [[ $1 = [Cc][Ss]* ]] || [[ $1 = -[Cc][Ss]* ]] || [[ $1 = --[Cc][Ss]* ]];then
