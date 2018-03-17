33fbf89434b24b9b2af691159e1ed8f5543dec21
S D Rausty <sdrausty@gmail.com>
2018-03-17 19:19:10 -0400

    Commit on 20180317 branch master 323248963

--- systemmaintenance.sh
+++ systemmaintenance.sh
@@ -110,8 +110,9 @@ refreshsys ()
 	printconfigq
 	rm root/bin/finishsetup.sh
 	rm root/bin/setupbin.sh 
-	printf "\033[1;34m  The following files have been updated to the newest version.\n\n\033[0m"
+	printf "\033[1;34m  The following files have been updated to the newest version.\n\n\033[0;32m"
 	ls $installdir/bin/ces |cut -f7- -d /
+	ls $installdir/bin/we |cut -f7- -d /
 	ls $installdir/$bin |cut -f7- -d /
 	ls $installdir/root/bin/* |cut -f7- -d /
 	printf "\n" 
