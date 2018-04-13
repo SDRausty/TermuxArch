88ac6b5c88fecef07c2bc6eadacce64463c3b229
S D Rausty <sdrausty@gmail.com>
2018-04-13 02:09:50 -0400

    Commit on 20180413 branch master 932812955

--- setupTermuxArch.sh
+++ setupTermuxArch.sh
@@ -685,12 +685,9 @@ wgetif ()
 	fi
 }
 
-setrootdir  
-namestartarch ret 
-nameinstalldir ret 
-
 # User configurable variables such as mirrors and download manager options are in `setupTermuxArchConfigs.sh`.  Creating this file from `kownconfigurations.sh` in the working directory is simple, use `setupTermuxArch.sh manual` to create, edit and run `setupTermuxArchConfigs.sh`; `setupTermuxArch.sh help` has more information.  All options can be abbreviated to the first letter(s). 
 
+commandif=$(command -v getprop) ||:
 declare -g args=$@
 #dfl=/gen
 dmverbose="-q"
@@ -698,6 +695,15 @@ dmverbose="-q"
 export PROOT_NO_SECCOMP=1
 stime=`date +%s|grep -o '....$'`
 versionid="v1.5"
+
+if [[ $commandif = "" ]];then
+	echo This is not an Android.
+	exit
+fi
+
+setrootdir ret 
+namestartarch ret 
+nameinstalldir ret 
 
 # [curl debug|curl sysinfo] Get device system information using `curl`.
 if [[ $1 = [Cc][Dd]* ]] || [[ $1 = -[Cc][Dd]* ]] || [[ $1 = --[Cc][Dd]* ]] || [[ $1 = [Cc][Ss]* ]] || [[ $1 = -[Cc][Ss]* ]] || [[ $1 = --[Cc][Ss]* ]];then
