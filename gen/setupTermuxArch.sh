730f6809f475fc9c6ab02cd3a0668f96a1411079
S D Rausty <sdrausty@gmail.com>
2018-04-14 20:26:03 -0400

    Commit on 20180414 branch master 621421001

--- gen/setupTermuxArch.sh
+++ gen/setupTermuxArch.sh
@@ -655,7 +655,7 @@ dmverbose="-q"
 #dmverbose="-v"
 export PROOT_NO_SECCOMP=1
 stime=`date +%s|grep -o '....$'`
-versionid="gen.v1.6 id943703395"
+versionid="gen.v1.6 id621421001"
 
 if [[ $commandif = "" ]];then
 	echo Run \`setupTermuxArch.sh\` from the Android system in Termux.
