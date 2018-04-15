c3b67c61aab49a58105fecee55dbae224713cb38
S D Rausty <sdrausty@gmail.com>
2018-04-14 22:06:50 -0400

    Commit on 20180414 branch master 210020151

--- scripts/files/working/systemmaintenance.sh
+++ scripts/files/working/systemmaintenance.sh
@@ -70,7 +70,7 @@ refreshsys ()
 {
 	printf '\033]2; setupTermuxArch.sh refresh 📲 \007'
 	if [ ! -d $installdir ] || [ ! -f $installdir/bin/we ];then
-		printf "\n\033[30mThe root directory structure is incorrect.  Refusing to continue \033[33mbash setupTermuxArch.sh refresh\033[30m.\033[0m\n"
+		printf "\n\033[0;33mThe root directory structure is incorrect.  Refusing to continue \033[1;33mbash setupTermuxArch.sh refresh\033[0;33m.\033[0m\n"
 		printtail 
 	else
 		cd $installdir
