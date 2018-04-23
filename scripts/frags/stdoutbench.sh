f2759ff24cd3c7368c6ec6504eab7e52ca26e2a3
S D Rausty <sdrausty@gmail.com>
2018-04-23 07:19:25 -0400

    Commit on 20180423 branch master 857609947

--- scripts/frags/stdoutbench.sh
+++ scripts/frags/stdoutbench.sh
@@ -3,7 +3,6 @@
 # Hosting https://sdrausty.github.io/TermuxArch courtesy https://pages.github.com
 # https://sdrausty.github.io/TermuxArch/CONTRIBUTORS Thank you for your help.  
 # https://sdrausty.github.io/TermuxArch/README has information about this project. 
-# Contributors to this file: @xeffyr @evg-zhabotinsky
 ################################################################################
 
 dd if=/dev/urandom bs=1K count=100 | hexdump -C >500KBfile.txt ; for i in {1..4}; do echo $i ; sleep 1 ; time nice -n 20 cat 500KBfile.txt ; sleep 2 ; done
