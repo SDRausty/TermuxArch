0b4762a35e421539ee3f96c62560af7804ffd29d
S D Rausty <sdrausty@gmail.com>
2018-05-06 17:38:53 -0400

    Commit on 20180506 branch master 739212657

--- scripts/files/stable/systemmaintenance.sh
+++ scripts/files/stable/systemmaintenance.sh
@@ -128,6 +128,6 @@ refreshsys () {
 	printfooter 
 	$installdir/$startbin ||:
 	printfooter2
-	printf "\n\033[0;32msetupTermuxArch.sh refresh \033[0m$versionid: \033[0;32mDONE 🏁  \n\n\033[0m"'\033]2; setupTermuxArch.sh refresh 🏁 \007'
+	printf "\n\033[0;32msetupTermuxArch.sh refresh \033[0m$versionid\033[1;32m: \033[0;32mDONE 🏁\n\n\033[0m"'\033]2; setupTermuxArch.sh refresh 🏁 \007'
 	exit
 }
