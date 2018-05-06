038270a34d347509ba52d2705a199424a57810ff
S D Rausty <sdrausty@gmail.com>
2018-05-06 11:18:26 -0400

    Commit on 20180506 branch master 091796908

--- necessaryfunctions.sh
+++ necessaryfunctions.sh
@@ -138,18 +138,18 @@ makefinishsetup () {
 		grep "proxy" $HOME/.profile | grep "export" >> root/bin/$binfnstp 2>/dev/null ||:
 	fi
 	if [ $cpuabi = $cpuabi5 ];then
-		printf "pacman -Rc linux-armv5 linux-firmware systemd --noconfirm --color always 2>/dev/null ||:\n" >> root/bin/$binfnstp ||:
+		printf "pacman -Rc linux-armv5 linux-firmware systemd --noconfirm --color always 2>/dev/null ||:\n" >> root/bin/$binfnstp
 	elif [ $cpuabi = $cpuabi7 ];then
-		printf "pacman -Rc linux-armv7 linux-firmware systemd --noconfirm --color always 2>/dev/null ||:\n" >> root/bin/$binfnstp ||:
+		printf "pacman -Rc linux-armv7 linux-firmware systemd --noconfirm --color always 2>/dev/null ||:\n" >> root/bin/$binfnstp
 	elif [ $cpuabi = $cpuabi8 ];then
-		printf "pacman -Rc linux-aarch64 linux-firmware systemd --noconfirm --color always 2>/dev/null ||:\n" >> root/bin/$binfnstp ||:
+		printf "pacman -Rc linux-aarch64 linux-firmware systemd --noconfirm --color always 2>/dev/null ||:\n" >> root/bin/$binfnstp
 	elif [ $cpuabi = $cpuabix86 ] || [ $cpuabi = $cpuabix8664 ];then
-		printf "pacman -Rc systemd --noconfirm --color always 2>/dev/null ||:\n" >> root/bin/$binfnstp ||:
+		printf "pacman -Rc systemd --noconfirm --color always 2>/dev/null ||:\n" >> root/bin/$binfnstp
 	fi
 	if [ $cpuabi = $cpuabix86 ] || [ $cpuabi = $cpuabix8664 ];then
-		printf "pacman -Syu sed --noconfirm --color always 2>/dev/null ||:\n" >> root/bin/$binfnstp ||:
+		printf "pacman -Syu sed --noconfirm --color always 2>/dev/null ||:\n" >> root/bin/$binfnstp
 	else
-		printf "pacman -Syu --noconfirm --color always 2>/dev/null ||:\n" >> root/bin/$binfnstp ||:
+		printf "pacman -Syu --noconfirm --color always 2>/dev/null ||:\n" >> root/bin/$binfnstp
 	fi
 	cat >> root/bin/$binfnstp <<- EOM
 	printf "\n\033[1;32m==> \033[0m"
