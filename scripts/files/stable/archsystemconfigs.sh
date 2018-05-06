cc04d24eb6a6492570e93616fad76a972786525d
S D Rausty <sdrausty@gmail.com>
2018-05-06 17:08:37 -0400

    Commit on 20180506 branch master 860337886

--- scripts/files/stable/archsystemconfigs.sh
+++ scripts/files/stable/archsystemconfigs.sh
@@ -306,7 +306,7 @@ addkeys () {
 
 addmotd () {
 	cat > etc/motd  <<- EOM
-	printf "\033[1;34mWelcome to Arch Linux in Termux!\nChat: \033[0mhttps://gitter.im/termux\n\033[1;34mHelp: \033[0;34minfo query \033[1;34mand \033[0;34mman query\n\033[1;34mIRC:  \033[0mhttps://wiki.archlinux.org/index.php/IRC_channel\n\n\033[1;34mInstall a package: \033[0;34mpacman -S package\n\033[1;34mMore  information: \033[0;34mpacman [-D|F|Q|R|S|T|U]h\n\033[1;34mSearch   packages: \033[0;34mpacman -Ss query\n\033[1;34mUpgrade  packages: \033[0;34mpacman -Syu\n\n\033[0m"
+	printf "\033[1;34mWelcome to Arch Linux in Termux!\nInstall a package: \033[0;34mpacman -S package\n\033[1;34mMore  information: \033[0;34mpacman [-D|F|Q|R|S|T|U]h\n\033[1;34mSearch   packages: \033[0;34mpacman -Ss query\n\033[1;34mUpgrade  packages: \033[0;34mpacman -Syu\n\n\033[1;34mChat: \033[0mhttps://gitter.im/termux\n\033[1;34mHelp: \033[0;34minfo query \033[1;34mand \033[0;34mman query\n\033[1;34mIRC:  \033[0mhttps://wiki.archlinux.org/index.php/IRC_channel\n\n\033[0m"
 	EOM
 }
 
@@ -428,7 +428,7 @@ addtour () {
 	printf "\n\033[1;32m==> \033[1;37mRunning \033[1;32mcat ~/.bashrc\033[1;37m\n\n"
 	cat ~/.bashrc
 	sleep 1
-	printf "\n\033[1;32m==> \033[1;37mShort tour is complete.  Run this script again at a later time, and you might be surprised at how the environment changes over time.  See http://tldp.org for information about Linux.\n\n"
+	printf "\n\033[1;32m==> \033[1;37mShort tour is complete; Run this script again at a later time, and it might be surprising at how this environment changes over time.  If you are new to *nix, see http://tldp.org for documentation.  \033[1;34mIRC:  \033[0mhttps://wiki.archlinux.org/index.php/IRC_channel\n\n"
 	EOM
 	chmod 770 root/bin/tour 
 }
