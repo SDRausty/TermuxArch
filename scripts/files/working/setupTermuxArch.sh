dd2b5f50548bbb452b18f99c0e50cfabf2ba8a81
S D Rausty <sdrausty@gmail.com>
2018-05-05 22:27:24 -0400

    Commit on 20180505 branch master 554952540

--- scripts/files/working/setupTermuxArch.sh
+++ scripts/files/working/setupTermuxArch.sh
@@ -653,7 +653,7 @@ dmverbose="-q"
 #dmverbose="-v"
 stime=`date +%s|grep -o '....$'`
 unset LD_PRELOAD
-versionid="gen.v1.6 id066048195"
+versionid="gen.v1.6 id554952540"
 
 if [[ $commandif = "" ]];then
 	echo Run \`setupTermuxArch.sh\` from the Android system in Termux.
