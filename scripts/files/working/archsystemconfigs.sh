763b1ff59e2451cfcedf9bc71b0f89e851d2ef60
S D Rausty <sdrausty@gmail.com>
2018-05-07 10:53:12 -0400

    Commit on 20180507 branch master 356580528

--- scripts/files/working/archsystemconfigs.sh
+++ scripts/files/working/archsystemconfigs.sh
@@ -306,7 +306,7 @@ addkeys () {
 
 addmotd () {
 	cat > etc/motd  <<- EOM
-	printf "\033[1;34mWelcome to Arch Linux in Termux!\nInstall a package: \033[0;34mpacman -S package\n\033[1;34mMore  information: \033[0;34mpacman [-D|F|Q|R|S|T|U]h\n\033[1;34mSearch   packages: \033[0;34mpacman -Ss query\n\033[1;34mUpgrade  packages: \033[0;34mpacman -Syu\n\n\033[1;34mHelp: \033[0;34minfo query \033[1;34mand \033[0;34mman query\n\033[1;34mIRC: \033[0mwiki.archlinux.org/index.php/IRC_channel\n\n\033[0m"
+	printf "\033[1;34mWelcome to Arch Linux in Termux!\nInstall a package: \033[0;34mpacman -S package\n\033[1;34mMore  information: \033[0;34mpacman [-D|F|Q|R|S|T|U]h\n\033[1;34mSearch   packages: \033[0;34mpacman -Ss query\n\033[1;34mUpgrade  packages: \033[0;34mpacman -Syu\n\n\033[1;34mChat: \033[0mhttps://gitter.im/termux\n\033[1;34mHelp: \033[0;34minfo query \033[1;34mand \033[0;34mman query\n\033[1;34mIRC:  \033[0mwiki.archlinux.org/index.php/IRC_channel\n\n\033[0m"
 	EOM
 }
 
