39e4d9fcd419f5cc03d1244bbfbedd2e53ec338d
sdrausty <sdrausty@gmail.com>
2018-07-25 04:20:22 -0400

    Commit on 20180725 branch master 398376910

--- gen/setupTermuxArch.sh
+++ gen/setupTermuxArch.sh
@@ -672,7 +672,7 @@ dmverbose="-q"
 stime=`date +%s|grep -o '....$'`
 trap finish SIGINT SIGTERM 
 unset LD_PRELOAD
-versionid="gen.v1.6 id656381842"
+versionid="gen.v1.6 id398376910"
 
 if [[ $commandif = "" ]];then
 	echo Run \`setupTermuxArch.sh\` from the Android system in Termux.
