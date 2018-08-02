1e54d0e878fbac4ed01841e08f43e85821f9b9fb
sdrausty <sdrausty@gmail.com>
2018-08-02 19:16:17 -0400

    Commit on 20180802 branch master 562684794504

--- gen/setupTermuxArch.sh
+++ gen/setupTermuxArch.sh
@@ -5,8 +5,8 @@
 # https://sdrausty.github.io/TermuxArch/README has information about TermuxArch. 
 ################################################################################
 
-set -euo pipefail 
-# set -euo pipeline
+# set -euo pipefail 
+set -eu
 unset LD_PRELOAD
 
 arg2dir () {
@@ -689,7 +689,7 @@ stim="$(date +%s)"
 stime="${stim:0:4}"
 # trap finish SIGINT SIGTERM 
 trap finish EXIT
-versionid="gen.v1.6 id155382124739"
+versionid="gen.v1.6 id562684794504"
 
 if [[ "$commandif" = "" ]];then
 	echo Run \`setupTermuxArch.sh\` from the Android system in Termux.
