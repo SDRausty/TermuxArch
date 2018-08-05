41f1dff3d3acfa05beb0345e1886b689be9f189a
sdrausty <sdrausty@gmail.com>
2018-08-05 09:15:29 -0400

    Commit on 20180805 branch master 810387711443

--- gen/setupTermuxArch.sh
+++ gen/setupTermuxArch.sh
@@ -706,7 +706,7 @@ stime="${stim:0:4}"
 trap finishs SIGINT SIGTERM 
 trap finisher ERR
 trap finishe EXIT
-versionid="gen.v1.6 id886945954142"
+versionid="gen.v1.6 id810387711443"
 
 if [[ "$commandif" = "" ]];then
 	echo Run \`setupTermuxArch.sh\` from the Android system in Termux.
--- scripts/files/working/systemmaintenance.sh
+++ scripts/files/working/systemmaintenance.sh
@@ -118,7 +118,7 @@ refreshsys () {
 	termux-wake-lock 
 	printdone 
 	printf '\033]2; setupTermuxArch.sh refresh 📲 \007'
-	printf "\\n\\e[1;32m==>  \\e[1;34mRunning \`bash setupTermuxArch.sh refresh\` 📲 \\e[0m\\a\\n"
+	printf "\\n\\e[1;32m==> \\e[0mRunning \`bash setupTermuxArch.sh refresh\` 📲 \\a\\n"
 	"$installdir"/root/bin/setupbin.sh 
 # 	printconfigup
 	rm -f root/bin/finishsetup.sh
