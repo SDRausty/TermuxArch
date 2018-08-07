56d67b2b959662e00951e1b4875ac7be32d88ebc
sdrausty <sdrausty@gmail.com>
2018-08-07 11:28:11 -0400

    Commit on 20180807 branch master 448465465574

--- gen/setupTermuxArch.sh
+++ gen/setupTermuxArch.sh
@@ -707,7 +707,7 @@ stime="${stim:0:4}"
 trap finisher ERR
 trap finishe EXIT
 trap finishs INT TERM 
-versionid="gen.v1.6 id659190844180"
+versionid="gen.v1.6 id448465465574"
 
 if [[ "$commandif" = "" ]];then
 	echo Run \`setupTermuxArch.sh\` from the Android system in Termux.
