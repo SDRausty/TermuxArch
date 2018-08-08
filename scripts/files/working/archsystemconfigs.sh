d116c528c5f0b4e163d28685a65eec8596ffb2a4
S D Rausty <sdrausty@gmail.com>
2018-08-07 22:03:01 -0400

    Commit on 20180807 branch master 040123123

--- scripts/files/working/archsystemconfigs.sh
+++ scripts/files/working/archsystemconfigs.sh
@@ -205,7 +205,7 @@ addch () { # Creates .hushlogin and .hushlogout file
 	# https://sdrausty.github.io/TermuxArch/README has information about this project. 
 	################################################################################
 	set -Eeou pipefail 
-versionid="gen.v1.6 id523500420"
+versionid="v1.6"
 
 	finishe () { # on exit
 		printf "\\e[?25h\\e[0m"
@@ -388,7 +388,7 @@ addkeys () {
 	set -Eeou pipefail 
 	shopt -s nullglob globstar
 	declare -a keyrings
-versionid="gen.v1.6 id523500420"
+versionid="v1.6"
 
 	finishe () { # on exit
 		printf "\\e[?25h\\e[0m"
@@ -488,7 +488,7 @@ addpc () { # pacman install packages shortcut
 	set -Eeou pipefail 
 	shopt -s nullglob globstar
 	declare -g args="\$@"
-versionid="gen.v1.6 id523500420"
+versionid="v1.6"
 
 	finishe () { # on exit
 		printf "\\e[?25h\\e[0m"
@@ -549,7 +549,7 @@ addpci () { # system update with pacman install packages shortcut
 	set -Eeuo pipefail 
 	shopt -s nullglob globstar
 	declare args="\$@"
-versionid="gen.v1.6 id523500420"
+versionid="v1.6"
 
 	finishe () { # on exit
 		printf "\\e[?25h\\e[0m"
