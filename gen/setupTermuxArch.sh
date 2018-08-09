7b4c85283a6088cb35cafd79d7f2d6aa1a2c638c
sdrausty <sdrausty@gmail.com>
2018-08-09 17:25:03 -0400

    Commit on 20180809 branch master 870796704474

--- gen/setupTermuxArch.sh
+++ gen/setupTermuxArch.sh
@@ -682,7 +682,7 @@ trap finishe EXIT
 trap finisher ERR
 trap finisher QUIT 
 trap finishs INT TERM 
-versionid="gen.v1.6 id236615366455"
+versionid="gen.v1.6 id870796704474"
 
 if [[ "$commandif" = "" ]];then
 	echo Run \`setupTermuxArch.sh\` from the Android system in Termux.
--- scripts/files/working/animationfunctions.sh
+++ scripts/files/working/animationfunctions.sh
@@ -14,6 +14,6 @@ endspin() {
    printf "\r%s\n" "$@"
 }
 
-while : ;do
-   spin 
-done
+# while : ;do
+#    spin 
+# done
