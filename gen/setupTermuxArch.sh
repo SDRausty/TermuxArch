69910335f7e6248980356cb1ebb0b885881c2b56
sdrausty <sdrausty@gmail.com>
2018-08-10 00:21:23 -0400

    Commit on 20180810 branch master 154162104994

--- gen/setupTermuxArch.sh
+++ gen/setupTermuxArch.sh
@@ -682,7 +682,7 @@ trap finishe EXIT
 trap finisher ERR
 trap finisher QUIT 
 trap finishs INT TERM 
-versionid="gen.v1.6 id014504224145"
+versionid="gen.v1.6 id154162104994"
 
 if [[ "$commandif" = "" ]];then
 	echo Run \`setupTermuxArch.sh\` from the Android system in Termux.
--- scripts/files/working/necessaryfunctions.sh
+++ scripts/files/working/necessaryfunctions.sh
@@ -297,7 +297,7 @@ md5check() {
 	if "$PREFIX"/bin/applets/md5sum -c "$file".md5 1>/dev/null ; then
 		printmd5success
 		printf "\\e[0;32m"
-		preproot & spinner "Uncompressing" "$file…" 
+		preproot & spinner "" "" ||: 
 	else
 		rmarchrm 
 		printmd5error
