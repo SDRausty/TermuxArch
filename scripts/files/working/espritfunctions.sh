3d365352b7e2e50435a5f4d7b4c48f1ef46d8705
sdrausty <sdrausty@gmail.com>
2018-08-10 00:34:19 -0400

    Commit on 20180810 branch master 685005404548

--- scripts/files/working/espritfunctions.sh
+++ scripts/files/working/espritfunctions.sh
@@ -13,7 +13,7 @@ spinner() { # Based on https://github.com/ringohub/sh-spinner
 	while :; do
 		jobs %1 > /dev/null 2>&1
 		[ $? = 0 ] || {
-			printf "\\e[0;32m ✓\\e[0m $task Done       \n"
+			printf "\\e[1;32m ✓ $task DONE       \\n\\e[0m"
 			break
 		}
 		for (( i=0; i<${#SPINNER}; i++ )); do
