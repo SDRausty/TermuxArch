4aec4183ccde1c2a1661ff8014bd329d2893425a
sdrausty <sdrausty@gmail.com>
2018-08-11 11:01:08 -0400

    Commit on 20180811 branch master 32375213943377

--- scripts/files/working/setupTermuxArch.sh
+++ scripts/files/working/setupTermuxArch.sh
@@ -687,7 +687,7 @@ trap finishe EXIT
 trap finisher ERR
 trap finisher QUIT 
 trap finishs INT TERM 
-versionid="gen.v1.6 id142187644935"
+versionid="v1.6"
 
 if [[ "$commandif" = "" ]];then
 	echo Run \`setupTermuxArch.sh\` from the Android system in Termux.
--- setupTermuxArch.sh
+++ setupTermuxArch.sh
@@ -9,8 +9,7 @@ set -Eeuo pipefail
 shopt -s nullglob globstar
 unset LD_PRELOAD
 
-## Functions ###################################################################
- 
+## Preliminary Functions #######################################################
 arg2dir() { 
 	arg2="${@:2:1}"
 	if [[ "$arg2" = "" ]] ;then
@@ -333,7 +332,7 @@ ldconf() {
 		. setupTermuxArchConfigs.sh
 		printconfloaded 
 	else
-		. knownconfigurations.sh
+		. knownconfigurations.sh 2>/dev/null
 	fi
 }
 
@@ -650,8 +649,13 @@ wgetif() {
 	fi
 }
 
-## Important ###################################################################
-# User configurable variables such as mirrors and download manager options are in `setupTermuxArchConfigs.sh`.  Creating this file from `kownconfigurations.sh` in the working directory is simple, use `setupTermuxArch.sh manual` to create, edit and run `setupTermuxArchConfigs.sh`; `bash setupTermuxArch.sh help` has more information.  
+## Important Information #######################################################
+## User configurable variables such as mirrors and download manager options \
+#+ are in `setupTermuxArchConfigs.sh`.  Creating this file from	\
+#+ `kownconfigurations.sh` in the working directory is simple, use \
+#+ `setupTermuxArch.sh manual` to create, edit and run \
+#+ `setupTermuxArchConfigs.sh`; See `bash setupTermuxArch.sh help` for more \
+#+ information.  
 
 commandif="$(command -v getprop)" ||:
 cpuabi="$(getprop ro.product.cpu.abi 2>/dev/null)" ||:
@@ -700,7 +704,7 @@ nameinstalldir
 namestartarch  
 setrootdir  
 
-################################################################################
+## Available Arguments #########################################################
 # []  Run default Arch Linux install.  `bash setupTermuxArch.sh help` has more information.  All options can be abbreviated to the first letter or two. 
 if [[ -z "${1:-}" ]];then
 	intro "$@" 
@@ -763,5 +767,4 @@ elif [[ "$1" = [Rr][Ee]* ]] || [[ "$1" = -[Rr][Ee]* ]] || [[ "$1" = --[Rr][Ee]* 
 else
 	printusage
 fi
-
-# EOF ##########################################################################
+## EOF #########################################################################
