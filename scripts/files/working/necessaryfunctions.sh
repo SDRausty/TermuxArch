b380af879034406cd6f98b2b563d8fbe21ec4f80
sdrausty <sdrausty@gmail.com>
2018-08-13 17:06:42 -0400

    Commit on 20180813 branch master 592360683428

--- scripts/files/working/necessaryfunctions.sh
+++ scripts/files/working/necessaryfunctions.sh
@@ -137,24 +137,24 @@ makefinishsetup() {
 	if [[ -e "$HOME"/.profile ]];then
 		grep "proxy" "$HOME"/.profile | grep "export" >> root/bin/"$binfnstp" 2>/dev/null ||:
 	fi
-# 	if [[ "$cpuabi" = "$cpuabi5" ]];then
-# 		printf "pacman -Rc linux-armv5 linux-firmware --noconfirm --color=always 2>/dev/null ||:\\n" >> root/bin/"$binfnstp"
-# 	elif [[ "$cpuabi" = "$cpuabi7" ]];then
-# 		printf "pacman -Rc linux-armv7 linux-firmware --noconfirm --color=always 2>/dev/null ||:\\n" >> root/bin/"$binfnstp"
-# 	elif [[ "$cpuabi" = "$cpuabi8" ]];then
-# 		printf "pacman -Rc linux-aarch64 linux-firmware --noconfirm --color=always 2>/dev/null ||:\\n" >> root/bin/"$binfnstp"
-# 	fi
+ 	if [[ "$cpuabi" = "$cpuabi5" ]];then
+ 		printf "pacman -Rc linux-armv5 linux-firmware --noconfirm --color=always 2>/dev/null ||:\\n" >> root/bin/"$binfnstp"
+ 	elif [[ "$cpuabi" = "$cpuabi7" ]];then
+ 		printf "pacman -Rc linux-armv7 linux-firmware --noconfirm --color=always 2>/dev/null ||:\\n" >> root/bin/"$binfnstp"
+ 	elif [[ "$cpuabi" = "$cpuabi8" ]];then
+ 		printf "pacman -Rc linux-aarch64 linux-firmware --noconfirm --color=always 2>/dev/null ||:\\n" >> root/bin/"$binfnstp"
+ 	fi
 	if [[ "$cpuabi" = "$cpuabix86" ]];then
 		printf "./root/bin/keys x86\\n" >> root/bin/"$binfnstp"
 	elif [[ "$cpuabi" = "$cpuabix86_64" ]];then
 		printf "./root/bin/keys x86_64\\n" >> root/bin/"$binfnstp"
 	else
-: # 		printf "./root/bin/keys\\n" >> root/bin/"$binfnstp"
+ 		printf "./root/bin/keys\\n" >> root/bin/"$binfnstp"
 	fi
 	if [[ "$cpuabi" = "$cpuabix86" ]] || [[ "$cpuabi" = "$cpuabix86_64" ]];then
 		printf "./root/bin/pci gzip sed \\n" >> root/bin/"$binfnstp"
 	else
-: # 		printf "./root/bin/pci \\n" >> root/bin/"$binfnstp"
+ 		printf "./root/bin/pci \\n" >> root/bin/"$binfnstp"
 	fi
 	cat >> root/bin/"$binfnstp" <<- EOM
 	printf "\\n\\e[1;32m==> \\e[0;32m"
