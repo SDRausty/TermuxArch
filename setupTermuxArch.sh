4a9dc49379a8ad15c9f30399f01deae58f07227a
sdrausty <sdrausty@gmail.com>
2018-08-16 09:55:43 -0400

    Commit on 20180816 branch master 90997045376197

--- setupTermuxArch.sh
+++ setupTermuxArch.sh
@@ -8,7 +8,7 @@ IFS=$'\n\t'
 set -Eeuo pipefail
 shopt -s nullglob globstar
 unset LD_PRELOAD
-versionid="v1.6 id0480"
+versionid="v1.6 id2224"
 
 ## Inaugural Functions #########################################################
 addcurl() {
@@ -677,7 +677,7 @@ declare cpuabi7="armeabi-v7a"
 declare cpuabi8="arm64-v8a"
 declare cpuabix86="x86"
 declare cpuabix86_64="x86_64"
-declare dfl=/gen # Used for development 
+declare dfl="" # Used for development 
 declare dm="curl"
 declare dmverbose="-q" # Use "-v" for verbose download manager output;  for verbose output throughout runtime, change in `knownconfigurations.sh` also, or in `setupTermuxArchConfigs.sh` if using `setupTermuxArch.sh manual`. 
 declare	ed=""
