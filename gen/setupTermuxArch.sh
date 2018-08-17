b517283311296583fe18cc60df4091ec6d436161
sdrausty <sdrausty@gmail.com>
2018-08-17 07:59:07 -0400

    Commit on 20180817 branch master 090055646450

--- gen/setupTermuxArch.sh
+++ gen/setupTermuxArch.sh
@@ -8,7 +8,7 @@ IFS=$'\n\t'
 set -Eeuo pipefail
 shopt -s nullglob globstar
 unset LD_PRELOAD
-versionid="gen.v1.6 id873114968433"
+versionid="gen.v1.6 id090055646450"
 
 ## Inaugural Functions #########################################################
 addcurl() {
