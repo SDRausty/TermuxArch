2087e04088328144e541c639d0484d41e8af6c70
sdrausty <sdrausty@gmail.com>
2018-08-17 15:02:53 -0400

    Commit on 20180817 branch master 703037054791

--- scripts/files/working/setupTermuxArch.sh
+++ scripts/files/working/setupTermuxArch.sh
@@ -8,7 +8,7 @@ IFS=$'\n\t'
 set -Eeo pipefail
 shopt -s nullglob globstar
 unset LD_PRELOAD
-versionid="gen.v1.6 id994571864418"
+versionid="gen.v1.6 id703037054791"
 
 ## Inaugural Functions #########################################################
 addcurl() {
@@ -179,8 +179,10 @@ dependsblock() {
 dwnl() {
 	if [[ "$dm" = aria2 ]];then
 		dmverbose=""
-		aria2c "$dmverbose" https://raw.githubusercontent.com/sdrausty/TermuxArch/master"$dfl"/setupTermuxArch.sha512 
-		aria2c "$dmverbose" https://raw.githubusercontent.com/sdrausty/TermuxArch/master"$dfl"/setupTermuxArch.tar.gz 
+# 		aria2c "$dmverbose" https://raw.githubusercontent.com/sdrausty/TermuxArch/master"$dfl"/setupTermuxArch.sha512 
+# 		aria2c "$dmverbose" https://raw.githubusercontent.com/sdrausty/TermuxArch/master"$dfl"/setupTermuxArch.tar.gz 
+		aria2c https://raw.githubusercontent.com/sdrausty/TermuxArch/master"$dfl"/setupTermuxArch.sha512 
+		aria2c https://raw.githubusercontent.com/sdrausty/TermuxArch/master"$dfl"/setupTermuxArch.tar.gz 
 		exit
 	elif [[ "$dm" = axel ]];then
 		dmverbose=""
