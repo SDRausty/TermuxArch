c8fe9397d9c17221bc1694b97964843a02ff4cd6
sdrausty <sdrausty@gmail.com>
2018-08-18 18:42:41 -0400

    Commit on 20180818 branch master 34706140523614

--- scripts/files/working/setupTermuxArch.sh
+++ scripts/files/working/setupTermuxArch.sh
@@ -8,7 +8,7 @@ IFS=$'\n\t'
 set -Eeuo pipefail
 shopt -s nullglob globstar
 unset LD_PRELOAD
-versionid="v1.6 id5803"
+versionid="v1.6 id3097"
 ## Init Functions ##############################################################
 addcurl() { # Adds `curl` to $PATH if not found.
 	cat > "$PREFIX"/bin/curl <<- EOM
@@ -177,7 +177,7 @@ depends() { # checks for missing commands.
 	apin "$aptin"
 # 	pe "$pins"
 	echo
-	echo "Using $dm to manage downloads." 
+	echo "Using ${dm:-curl} to manage downloads." 
 	printf "\\n\\e[0;34m 🕛 > 🕧 \\e[1;34mPrerequisites: \\e[1;32mOK  \\e[1;34mDownloading TermuxArch…\\n\\n\\e[0;32m"
 }
 
