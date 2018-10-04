1e7245deccba730f34370adb99a27c55261cf7de
SDRausty <SDRausty@gmail.com>
2018-10-04 10:20:16 -0400

    Commit on 20181004 branch master 00710466874011

--- setupTermuxArch.sh
+++ setupTermuxArch.sh
@@ -8,7 +8,7 @@ IFS=$'\n\t'
 set -Eeuo pipefail
 shopt -s nullglob globstar
 unset LD_PRELOAD
-VERSIONID="v1.6.4.id8693"
+VERSIONID="v1.6.4.id4144"
 
 _STRPERROR_() { # Run on script error.
 	local RV="$?"
