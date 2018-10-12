122cffb1b1cfc833157b5506d9ef2b8757708227
SDRausty <SDRausty@gmail.com>
2018-10-12 10:25:25 -0400

    Commit on 20181012 branch master 16456141898306

--- knownconfigurations.sh
+++ knownconfigurations.sh
@@ -59,32 +59,34 @@ _X86_64_() { # $file is read from md5sums.txt
 ## To regenerate the start script use \`setupTermuxArch.sh re[fresh]\`.  An example is included for convenience.  Usage: PROOTSTMNT+=\"-b host_path:guest_path \" The space before the last double quote is necessary.  Appending to the PRoot statement can be accomplished on the fly by creating a *.prs file in /var/binds.  The format is straightforward, `PROOTSTMNT+="option command "`.  The space is required before the last double quote.  `info proot` and `man proot` have more information about what can be configured in a proot init statement.  `setupTermuxArch.sh manual refresh` will refresh the installation globally.  If more suitable configurations are found, share them at https://github.com/sdrausty/TermuxArch/issues to improve TermuxArch.  
 
 _PR00TSTRING_() { 
-PROOTSTMNT="exec proot "
-if [[ -z "${KID:-}" ]] ; then
-	PROOTSTMNT+=""
-elif [[ "$KID" ]] ; then
- 	PROOTSTMNT+="--kernel-release=4.14.15 "
-fi
-if [[ "${KOE:-}" ]] ; then
-	PROOTSTMNT+="--kill-on-exit "
-fi
-PROOTSTMNT+="--link2symlink -0 -r $INSTALLDIR "
-if [[ ! -r /dev/ashmem ]] ; then
-	PROOTSTMNT+="-b $INSTALLDIR/tmp:/dev/ashmem " 
-fi
-if [[ ! -r /dev/shm ]] ; then
-	PROOTSTMNT+="-b $INSTALLDIR/tmp:/dev/shm " 
-fi
-if [[ ! -r /proc/stat ]] ; then
-	PROOTSTMNT+="-b $INSTALLDIR/var/binds/fbindprocstat:/proc/stat " 
-fi
-if [[ -n "$(ls -A "$INSTALLDIR"/var/binds/*.prs)" ]] ; then
-    for PRSFILES in "$INSTALLDIR"/var/binds/*.prs ; do
-      . "$PRSFILES"
-    done
-fi
-PROOTSTMNT+="-b \"\$ANDROID_DATA\" -b /dev/ -b \"\$EXTERNAL_STORAGE\" -b \"\$HOME\" -b /proc/ -b /storage/ -b /sys/ -w \"\$PWD\" /usr/bin/env -i HOME=/root TERM=$TERM "
-PROOTSTMNTU="${PROOTSTMNT//--link2symlink }"
+	PROOTSTMNT="exec proot "
+       	if [[ -z "${KID:-}" ]] ; then
+	       	PROOTSTMNT+=""
+       	elif [[ "$KID" ]] ; then
+	       	PROOTSTMNT+="--kernel-release=4.14.15 "
+       	fi
+       	if [[ "${KOE:-}" ]] ; then
+	       	PROOTSTMNT+="--kill-on-exit "
+       	fi
+       	PROOTSTMNT+="--link2symlink -0 -r $INSTALLDIR "
+       	if [[ ! -r /dev/ashmem ]] ; then
+	       	PROOTSTMNT+="-b $INSTALLDIR/tmp:/dev/ashmem " 
+	fi
+       	if [[ ! -r /dev/shm ]] ; then
+	       	PROOTSTMNT+="-b $INSTALLDIR/tmp:/dev/shm " 
+	fi
+       	if [[ ! -r /proc/stat ]] ; then
+	       	PROOTSTMNT+="-b $INSTALLDIR/var/binds/fbindprocstat:/proc/stat " 
+	fi
+       	if [[ -n "$(ls -A "$INSTALLDIR"/var/binds/*.prs)" ]] ; then
+	       	for PRSFILES in "$INSTALLDIR"/var/binds/*.prs ; do
+		       	. "$PRSFILES"
+	       	done
+       	fi
+       	PROOTSTMNT+="-b /proc/self/fd/1:/dev/stdout "
+       	PROOTSTMNT+="-b /proc/self/fd/2:/dev/stderr "
+       	PROOTSTMNT+="-b \"\$ANDROID_DATA\" -b /dev/ -b \"\$EXTERNAL_STORAGE\" -b \"\$HOME\" -b /proc/ -b /storage/ -b /sys/ -w \"\$PWD\" /usr/bin/env -i HOME=/root TERM=$TERM "
+       	PROOTSTMNTU="${PROOTSTMNT//--link2symlink }"
 }
 _PR00TSTRING_
 
