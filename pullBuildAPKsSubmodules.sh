62bffad74f4444e6a0cc81c8fc34803bd882e833
SDRausty <sdrausty@gmail.com>
2019-01-02 03:04:49 -0500

    new file:   pullBuildAPKsSubmodules.sh

---
+++ pullBuildAPKsSubmodules.sh
@@ -0,0 +1,8 @@
+#!/data/data/com.termux/files/usr/bin/sh -e 
+# Copyright 2017 (c) all rights reserved 
+# by S D Rausty https://sdrausty.github.io
+# Update repository and update submodules.
+#####################################################################
+cd $HOME/buildAPKs
+git pull 
+git submodule update --init --recursive --remote
