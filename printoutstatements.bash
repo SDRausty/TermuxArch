a1381daa62c7aca52adeca77160b35abeb0448cc
SDRausty <27742457+SDRausty@users.noreply.github.com>
2020-08-27 21:33:20 -0400

    commit 256977525 on 20200827

--- printoutstatements.bash
+++ printoutstatements.bash
@@ -15,7 +15,7 @@ FLHDR1[1]="# IFS=$'\\n\\t'"
 FLHDR1[2]="set -Eeuo pipefail"
 FLHDR1[3]="shopt -s nullglob globstar"
 FLHDR1[4]="unset LD_PRELOAD"
-FLHDR1[5]="VERSIONID=2.0.123"
+FLHDR1[5]="VERSIONID=2.0.124"
 FLHDR1[6]=" "
 FLHDRP[0]="## BEGIN #####################################################################"
 FLHDRP[1]=""
@@ -215,7 +215,7 @@ _PRINTPROOTERROR_() {
 
 _PRINTROOTDIRFUNCTION_() {
 	declare -g PRINTROOTDIR
-	PRINTROOTDIR="$(printf "%s" "${ROOTDIR%/}" | sed 's#//*#/#g')"
+	PRINTROOTDIR="$(printf "%s" "${ROOTDIR%/}"|sed 's#//*#/#g')"
 }
 _PRINTROOTDIRFUNCTION_
 
@@ -223,5 +223,6 @@ _PRINTROOTDIRFUNCTION_
 _PSGI1ESTRING_() {
 	printf "\\e[1;31m%s\\e[1;37m%s\\e[1;31m%s\\e[1;37m%s\\n\\n\\e[0m" "Signal generated in '$1' : cannot complete '$1' : continuing...   Running " "bash ${0##*/} refresh" " will attempt to complete the autoconfiguration and installation." "  If you find improvements for '${0##*/}' please open an issue and accompanying pull request." 
 }
-# _PSGI1ESTRING_ "test string" && exit # print signal generated with arg 1 format
+# print signal generated with arg 1 format
+# _PSGI1ESTRING_ "test string" && exit
 # printoutstatements.bash EOF
