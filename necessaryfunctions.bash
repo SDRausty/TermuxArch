9dbf097eab333d47795379cc542180aafb7e42e5
SDRausty <27742457+SDRausty@users.noreply.github.com>
2020-08-28 13:34:43 -0400

    commit 553736560 on 20200828

--- necessaryfunctions.bash
+++ necessaryfunctions.bash
@@ -390,7 +390,7 @@ _PREPINSTALLDIR_() {
 	_MAKESETUPBIN_
 	_MAKESTARTBIN_
 	_FIXOWNER_
-  	[[ $ELCR == 0 ]] && exit || _PSGI1ESTRING_ "_PREPINSTALLDIR_ necessaryfunctions.bash ${0##*/}" # create TermuxArchBloom and Arch Linux in Termux PRoot root directories skeleton with files.  Then exit without installing the root file system;  Commands 'setupTermuxArch b[l[oom]]' can be used to access these features.
+  	[[ $ELCR == 0 ]] && exit # create TermuxArchBloom and Arch Linux in Termux PRoot root directories skeleton.  These options do not install the root file system;  Commands 'setupTermuxArch b[l[oom]]' can be used to access these features.
 }
 
 _PREPROOT_() {
