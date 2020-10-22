c452730b52619c5873c03b7cd94cbf1c8508ab9e
SDRausty <27742457+SDRausty@users.noreply.github.com>
2020-10-22 16:06:34 -0400

    commit 709053921 on 20201022

--- setupTermuxArch.sh
+++ setupTermuxArch.sh
@@ -4,7 +4,7 @@
 # https://termuxarch.github.io/TermuxArch/CONTRIBUTORS thank you for helping
 # command 'setupTermuxArch h[elp]' has information how to use this file
 ################################################################################
-VERSIONID=2.0.236
+VERSIONID=2.0.237
 IFS=$'\n\t'
 set -Eeuo pipefail
 shopt -s nullglob globstar
