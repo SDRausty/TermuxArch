793fdd4b10ba14f116453ea51b3b667b942ba9e9
SDRausty <27742457+SDRausty@users.noreply.github.com>
2020-11-09 13:58:00 -0500

    commit 542444527 on 20201109

--- archlinuxconfig.bash
+++ archlinuxconfig.bash
@@ -624,8 +624,9 @@ pacman -S pacman --noconfirm || _PRTERROR_
 rm /etc/ssl/certs/ca-certificates.crt
 printf \"\\n\\e[1;32m==>  \\e[1;37mRunning \${0##*/} [6/7] $ARCHITEC ($CPUABI) architecture upgrade ; \\e[1;32m%s\\e[0m...\\n\" \"pacman -S ca-certificates\"
 pacman -S ca-certificates || _PRTERROR_
-printf \"\\n\\e[1;32m==>  \\e[1;37mRunning \${0##*/} [6/7] $ARCHITEC ($CPUABI) architecture upgrade ; \\e[1;32m%s\\e[0m...\\n\" \"pacman -S bash ca-certificates-utils ca-certificates-mozilla coreutils gcc-libs glibc gpgme grep gzip libffi linux-api-headers ncurses openssl patch readline sed sudo tzdata unzip zstd --noconfirm\"
-pacman -S bash ca-certificates-utils ca-certificates-mozilla coreutils gcc-libs glibc gpgme grep gzip libffi linux-api-headers ncurses openssl patch readline sed sudo tzdata unzip zstd --noconfirm || _PRTERROR_
+printf \"\\n\\e[1;32m==>  \\e[1;37mRunning \${0##*/} [6/7] $ARCHITEC ($CPUABI) architecture upgrade ; \\e[1;32m%s\\e[0m...\\n\" \"pacman -S awk bash ca-certificates-utils ca-certificates-mozilla coreutils gcc-libs glibc gpgme grep gzip libffi linux-api-headers ncurses openssl patch readline sed sudo tzdata unzip zstd --noconfirm\"
+pacman -S awk bash ca-certificates-utils ca-certificates-mozilla coreutils gcc-libs glibc gpgme grep gzip libffi linux-api-headers ncurses openssl patch readline sed sudo tzdata unzip zstd --noconfirm || _PRTERROR_
+locale-gen || _PRTERROR_
 printf \"\\n\\e[1;32m==>  \\e[1;37mRunning \${0##*/} [7/7] $ARCHITEC ($CPUABI) architecture upgrade ; \\e[1;32m%s\\e[0m...\\n\" \"pacman -Su --noconfirm ; Starting full system upgrade\"
 pacman -Su --noconfirm || _PRTERROR_"
 X86IPT=" "
--- setupTermuxArch.bash
+++ setupTermuxArch.bash
@@ -5,7 +5,7 @@
 # command 'setupTermuxArch h[elp]' has information how to use this file
 ################################################################################
 IFS=$'\n\t'
-VERSIONID=2.0.353
+VERSIONID=2.0.354
 set -Eeuo pipefail
 shopt -s nullglob globstar
 umask 0022
