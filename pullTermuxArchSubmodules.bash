f0a53fae75e60b4a5e322238bc0594bcf477c592
SDRausty <27742457+SDRausty@users.noreply.github.com>
2022-02-17 20:46:20 -0500

    commit 972626531 on 20220217

--- pullTermuxArchSubmodules.bash
+++ pullTermuxArchSubmodules.bash
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-## Copyright 2017-2022 (c) by TermuxArch, all rights reserved, see LICENSE 🌎 🌍 🌏 🌐 🗺
+## Copyright 2017-2021 (c) by TermuxArch, all rights reserved, see LICENSE 🌎 🌍 🌏 🌐 🗺
 ## https://termuxarch.github.io/TermuxArch courtesy host https://pages.github.com
 ## update git repository, then update modules
 ################################################################################
@@ -35,27 +35,27 @@ trap _SGSATRPEXIT_ EXIT
 trap _SGSATRPSIGNAL_ HUP INT TERM
 trap _SGSATRPQUIT_ QUIT
 
-RDR="$PWD"
+RDR="$(pwd)"
 _GSA_() { # git repository update modules
 WRDR="$1"
 (git submodule update $3 --depth 1 --init --recursive --remote "$1" && _PRCS_) || _PESTRG_ "$1" update # the command ` git submodule help ` and the book https://git-scm.com/book/en/v2/Git-Tools-Submodules have more information about git submodules
 }
 
 _PESTRG_() {
-printf "\\n\\n%s\\n" "Cannot $2 module $1; Continuing..."
+printf "\\n\\n%s\\n" "Cannot $2 module $1 : Continuing..."
 }
 
 _PRCS_ () {	# print checksums message and run sha512sum
-cd "$WRDR"
+cd $WRDR
 if [[ -f sha512.sum ]]
 then
-_PRT_ "Checking checksums in directory $PWD with sha512sum: "
+_PRT_ "Checking checksums in direcory $(pwd) with sha512sum: "
 sha512sum -c --quiet sha512.sum 2>/dev/null || printf "%s\\n" "sha512sum -c sha512.sum FAILED!"
 _PRNT_  "DONE"
 else
-printf "%s\\n" "No 'sha512.sum' file found in directory $PWD."
+printf "%s\\n" "No 'sha512.sum' file found in directory $(pwd)."
 fi
-cd "$RDR"
+cd $RDR
 sleep 0."$(shuf -i 24-72 -n 1)" # latency support
 }
 
@@ -67,17 +67,13 @@ _PRNT_ () {	# print message with one trialing newline
 printf "%s\\n" "$1"
 }
 
-git pull || printf "\\n\\n%s\\n" "Cannot git pull; Continuing..."
-if grep '\.\/\.git\/' sha512.sum 1>/dev/null || grep '\.\/\.scripts\/maintenance\/' sha512.sum 1>/dev/null || grep '\.\/docs\/' sha512.sum 1>/dev/null || grep '\.\/gen\/' sha512.sum 1>/dev/null
-then
-sed -i '/\.\/\.git\//d' sha512.sum
-sed -i '/\.\/\.scripts\/maintenance\//d' sha512.sum
-sed -i '/\.\/docs\//d' sha512.sum
-sed -i '/\.\/gen\//d' sha512.sum
-fi
-sha512sum -c --quiet sha512.sum || _PRNT_ "WARNING:  Checking checksums in directory $PWD with sha512sum FAILED! "
-_GSA_ ".scripts/maintenance" maintenance "" || printf "\\n\\n%s\\n" "Cannot add or update module .scripts/maintenance; Continuing..."
-_GSA_ docs docsTermuxArch "" || printf "\\n\\n%s\\n" "Cannot add or update module docs; Continuing..."
-_GSA_ gen genTermuxArch "" || printf "\\n\\n%s\\n" "Cannot add or update module gen; Continuing..."
-_GSA_ scripts "scripts.TermuxArch" "" || printf "\\n\\n%s\\n" "Cannot add or update module scripts; Continuing..."
-# pullTermuxArchSubmodules.bash FE
+git pull || printf "\\n\\n%s\\n" "Cannot git pull : Continuing..."
+sha512sum -c --quiet sha512.sum || _PRNT_ "WARNING:  Checking checksums in direcory $(pwd) with sha512sum FAILED! "
+SIAD="$(grep url .git/config|cut -d"=" -f 2|head -n 1|cut -d"/" -f 2-3)"
+OUNA="/shlibs"
+_GSA_ ".scripts/maintenance" maintenance "" || printf "\\n\\n%s\\n" "Cannot add or update module .scripts/maintenance : Continuing..."
+OUNA="/TermuxArch"
+_GSA_ docs docsTermuxArch "" || printf "\\n\\n%s\\n" "Cannot add or update module docs : Continuing..."
+_GSA_ gen genTermuxArch "" || printf "\\n\\n%s\\n" "Cannot add or update module gen : Continuing..."
+_GSA_ scripts "scripts.TermuxArch" "" || printf "\\n\\n%s\\n" "Cannot add or update module scripts : Continuing..."
+# pullTermuxArchSubmodules.bash EOF
