8d72c395905bb225ea841657474400355db69246
SDRausty <27742457+SDRausty@users.noreply.github.com>
2022-03-14 18:16:30 -0400

    commit 049394995 on 20220314

--- necessaryfunctions.bash
+++ necessaryfunctions.bash
@@ -186,12 +186,12 @@ _MAKESYSTEM_() {
 _WAKELOCK_
 if [ "$USECACHEDIR" = 0 ] && [ -z "${LCR:-}" ]
 then
-printf '\e[0;32mPopulating from cache files;  \e[1;32mBEGUN\n'
-{ cd "$CACHEDIR" && printf '%s' "cd $CACHEDIR && " ; } || { cd "$PREFIXDATAFILES" && mkdir -p "$CACHEDIRSUFIX" && cd "$CACHEDIR" && printf '%s' "cd $PREFIXDATAFILES && mkdir -p $CACHEDIRSUFIX && cd $CACHEDIR && " ; } || exit 196
-printf '%s\n' "cp -fr * $INSTALLDIR"
-cp -fr * "$INSTALLDIR"
-cd "$INSTALLDIR" && printf '%s\n' "cd $INSTALLDIR" || exit 196
-printf '\e[0;32mPopulating from cache files;  \e[1;32mDONE\n\n'
+if [ -d "$CACHEDIR" ]
+then
+printf '\e[0;32mPopulating from cache files;  \e[1;32mBEGUN\n' &&  cd "$CACHEDIR" && printf '%s' " cd $CACHEDIR && cp -fr * $INSTALLDIR; Please wait...  " && cp -fr ./* "$INSTALLDIR" && cd "$INSTALLDIR" && printf '%s\n' "cd $INSTALLDIR" && printf '\e[0;32mPopulating from cache files;  \e[1;32mDONE\n\n'
+else
+cd "$PREFIXDATAFILES" && { [ -d "$CACHEDIRSUFIX" ] || mkdir -p "$CACHEDIRSUFIX" ; } && printf '%s' "cd $PREFIXDATAFILES && mkdir -p $CACHEDIRSUFIX && cd $CACHEDIR" || printf '%s\n\n' "Please create cache directory '$CACHEDIR' in order to use the cache directory feature;  Continuing..."
+fi
 fi
 _CALLSYSTEM_
 _MD5CHECK_
